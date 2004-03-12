9404f160065ca65f8eb90540abc229acf5b992be
Martin Schoeberl <martin@jopdesign.com>
2004-03-12 13:41:13 +0000

    update all changes from my src tree.

--- java/target/doit.bat
+++ java/target/doit.bat
@@ -6,7 +6,7 @@ rem jbi32 -dDO_PROGRAM=1 -aPROGRAM ..\..\jbc\acxmin_init.jbc
 rem jbi32 -dDO_PROGRAM=1 -aCONFIGURE ..\..\jbc\acxmin_init.jbc
 rem jbi32 -dDO_PROGRAM=1 -aCONFIGURE ..\..\jbc\acxtal_init.jbc
 rem jbi32 -dDO_PROGRAM=1 -aPROGRAM ..\..\jbc\bgcyc_init.jbc
-rem jbi32 -dDO_PROGRAM=1 -aCONFIGURE ..\..\jbc\bgcyc_init.jbc
+jbi32 -dDO_PROGRAM=1 -aCONFIGURE ..\..\jbc\bgcyc_init.jbc
 ..\..\down -e dist\bin\%project%.bin COM2
 
 rem java -cp pc Flash %project%.bin 192.168.1.2
