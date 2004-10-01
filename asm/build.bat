9055b1e22a23ef0346866f6379b2c93704572192
Martin Schoeberl <martin@jopdesign.com>
2004-10-01 13:05:24 +0000

    no message

--- asm/build.bat
+++ asm/build.bat
@@ -2,9 +2,9 @@ set project=%1
 set blocksize=4096
 rem generated is used by jop[ser|flash].bat
 rem rmdir /Q /S generated
-mkdir generated
+rem mkdir generated
 java -cp ..\java\tools\dist\lib\jop-tools.jar com.jopdesign.tools.Jopa -s src -d generated %project%.asm
 rem java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -d 1024 -w 8 -m xjbc_block -o generated
-rem java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -m xram_block generated\ram.mif generated\xram_block.vhd
+java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -m xram_block generated\ram.mif generated\xram_block.vhd
 rem java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -m xrom_block generated\rom.mif generated\xrom_block.vhd
 copy generated\*.vhd ..\vhdl
