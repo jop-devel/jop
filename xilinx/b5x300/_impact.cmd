8ff86960386070d815d90dcd1f2442654977d165
Martin Schoeberl <martin@jopdesign.com>
2004-10-01 13:04:48 +0000

    *** empty log message ***

--- xilinx/b5x300/_impact.cmd
+++ xilinx/b5x300/_impact.cmd
@@ -16,21 +16,3 @@ setPreference -pref StartupCLock:AUTO_CORRECTION
 setPreference -pref AutoSignature:FALSE
 setPreference -pref KeepSVF:FALSE
 setPreference -pref svfUseTime:FALSE
-setMode -bs
-setCable -port auto
-Identify
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\testmon\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\testmon\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\testmon\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\jvmmon\jop.bit"
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\jvmmon\jop.bit"
-Program -p 1 
-setAttribute -position 1 -attr configFileName -value "F:\Projects\JOP\b5x300\builds\testmon\jop.bit"
-Program -p 1 
