5bca136e38c0e4113d94d79b1639121bee5af9aa
Martin Schoeberl <martin@jopdesign.com>
2005-05-30 16:42:08 +0000

    JOP is now a Wishbone master!

---
+++ java/target/src/test/wishbone/Simple.java
@@ -0,0 +1,53 @@
+/*
+ * Created on 30.05.2005
+ *
+ */
+package wishbone;
+
+import com.jopdesign.sys.Const;
+import com.jopdesign.sys.Native;
+
+/**
+ * @author admin
+ *
+ */
+public class Simple {
+
+	public static void main(String[] args) {
+		
+		int t1, t2;
+		int i, j;
+		
+		System.out.println("Wishbone counter:");
+		for (i=0; i<10; ++i) {
+			System.out.println(Native.rdMem(Const.WB_TS0));			
+		}
+		System.out.println();
+		
+		System.out.print("Wishbone slave 0: ");
+		System.out.println(Native.rdMem(Const.WB_TS0+1));
+		System.out.print("Wishbone slave 1: ");
+		System.out.println(Native.rdMem(Const.WB_TS1+1));
+		
+		System.out.println("Writing values to the slaves...");
+		Native.wrMem(123, Const.WB_TS0+1);
+		Native.wrMem(456, Const.WB_TS1+1);
+
+		System.out.print("Wishbone slave 0: ");
+		System.out.println(Native.rdMem(Const.WB_TS0+1));
+		System.out.print("Wishbone slave 1: ");
+		System.out.println(Native.rdMem(Const.WB_TS1+1));
+		
+
+		t1 = Native.rd(Const.IO_CNT);
+		t2 = Native.rd(Const.IO_CNT);
+		System.out.print("JOP counter: ");
+		System.out.println(t2-t1);
+
+		t1 = Native.rdMem(Const.WB_TS0);
+		t2 = Native.rdMem(Const.WB_TS1);
+		System.out.print("Wishbone counter: ");
+		System.out.println(t2-t1);
+
+	}
+}
