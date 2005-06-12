8744e52e50c15754de151a6dbffec0af4f628231
Martin Schoeberl <martin@jopdesign.com>
2005-06-12 11:54:55 +0000

    JOPizer is now the default JOP build tool

--- java/tools/build.bat
+++ java/tools/build.bat
@@ -3,7 +3,8 @@ mkdir dist\lib
 mkdir dist\classes
 dir /b /s ..\target\src\common\com\jopdesign\sys\Const.java > .sourcefiles
 dir /b /s src\*.java >> .sourcefiles
-javac -g -d dist\classes -classpath jcc.jar @.sourcefiles
+set java_lib=../tools/dist/lib/jop-tools.jar;../lib/bcel-5.1.jar;../lib/jakarta-regexp-1.3.jar;jcc.jar
+javac -g -d dist\classes -classpath %java_lib% @.sourcefiles
 del .sourcefiles
 cd dist\classes
 jar cf ..\lib\jop-tools.jar *
