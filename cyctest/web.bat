342cb8423a18804981dcf0857b92d5eed69cda96
Martin Schoeberl <martin@jopdesign.com>
2005-11-25 09:35:29 +0000

    clean modelsim directory

--- cyctest/web.bat
+++ cyctest/web.bat
@@ -2,6 +2,9 @@ rem xxx
 cd ..\..
 xcopy /s jop \tmp\jop\
 cd \tmp\jop
+cd modelsim
+call clean
+cd ..
 for /R . %%x IN (*.jop) DO del %%x
 for /R . %%x IN (*.ttf) DO del %%x
 for /R . %%x IN (*.sof) DO del %%x
