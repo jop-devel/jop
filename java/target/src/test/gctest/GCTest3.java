3250b30b9b4da083b6f3e5e64ee60fa9080ff109
Martin Schoeberl <martin@jopdesign.com>
2005-12-30 16:23:30 +0000

    Stack walker by Rasmus

---
+++ java/target/src/test/gctest/GCTest3.java
@@ -0,0 +1,72 @@
+package gctest;
+
+import joprt.RtThread;
+import util.Dbg;
+import com.jopdesign.sys.*;
+
+// A test of the GCStackWalker
+// It calls the gcsw from the main thread, the main thread
+//   from the "GC" thread, the "GC" thread from the "GC" thread
+// Status: It is supposed to (when ready) return the potential
+//   references. It is perhaps a slight improvement over a total
+//   stack walk.
+
+// RtThread needs these three methods
+//public static int[] getStack(int num){
+//	return ref[num].stack;
+//}
+//
+//public static int getSP(int num){
+//	return ref[num].sp;
+//}
+//
+//public static int getActive(){
+//	return active;
+//}
+
+public class GCTest3 {
+
+	public static void main(String s[]) {
+	  
+	  GCStkWalk.init();
+      
+	  SomeClass1 sc1 = new SomeClass1();
+	  SomeClass2 sc2 = new SomeClass2(sc1);
+System.exit(0);
+	  
+	  new RtThread(20, 100000) {
+			public void run() {
+				for (;;) {
+					System.out.println("Walk main from \"GC\" thread");
+					GCStkWalk.swk(0);
+//					System.out.println("Walk \"GC\" thread from \"GC\" thread");
+//					GCStkWalk.swk();
+                    System.exit(0);
+					waitForNextPeriod();
+				}
+			}
+		};
+		RtThread.startMission();
+		for(;;){
+			RtThread.sleepMs(1000);
+		}
+	}
+}
+class SomeClass1 {
+	int i,j;
+	SomeClass1 sc1inst;  
+	void someMethod(){
+		
+		sc1inst = this;
+		int ii = i+j;
+		SomeClass1 sc1instlocal = this;
+		System.out.println("Walk someMethod");
+		GCStkWalk.swk(0);
+	  }
+
+}
+class SomeClass2 {
+	public SomeClass2(SomeClass1 sc1){
+		sc1.someMethod();
+	}
+}
