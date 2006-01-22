7c9a2e17c81d81eb3502863e0ed8b5c2d8a4d1d2
Martin Schoeberl <martin@jopdesign.com>
2006-01-22 20:14:02 +0000

    correct CPI index for new field instructions

--- java/tools/src/com/jopdesign/build/ReplaceNativeAndCPIdx.java
+++ java/tools/src/com/jopdesign/build/ReplaceNativeAndCPIdx.java
@@ -56,6 +56,7 @@ public class ReplaceNativeAndCPIdx extends MyVisitor {
 		InstructionList il  = mg.getInstructionList();
 		InstructionFinder f = new InstructionFinder(il);
 		
+//		System.out.println("Replace: "+method.getName());
 		// find invokes first and replace call to Native by
 		// JOP native instructions.
 		String invokeStr = "InvokeInstruction";		
@@ -74,54 +75,12 @@ public class ReplaceNativeAndCPIdx extends MyVisitor {
 			}
 		}
 
-		// Added by Rasmus and extended by Martin
+		// Added field instruction replacement
+		// by Rasmus and extended by Martin
 		// Replace reference and long/double field bytecodes
 		// with 'special' bytecodes.
 		// TODO: also replace index by the offset into the object
 		// on method fields
-		f = new InstructionFinder(il);
-
-		String fs = "FieldInstruction";
-		for (Iterator i = f.search(fs); i.hasNext();) {
-			InstructionHandle[] match = (InstructionHandle[]) i.next();
-			for (int j = 0; j < match.length; j++) {
-				InstructionHandle ih = match[j];
-				FieldInstruction fi = (FieldInstruction) ih.getInstruction();
-				int index = fi.getIndex();
-				Type ft = fi.getFieldType(cpool);
-				
-				boolean isRef = ft instanceof ReferenceType;
-				boolean	isLong = ft==BasicType.LONG || ft==BasicType.DOUBLE;
-
-				if (fi instanceof GETSTATIC) {
-					if (isRef) {
-						ih.setInstruction(new GETSTATIC_REF((short) index));
-					} else if (isLong) {
-						ih.setInstruction(new GETSTATIC_LONG((short) index));
-					}
-				} else if (fi instanceof PUTSTATIC) {
-					if (isRef) {
-						ih.setInstruction(new PUTSTATIC_REF((short) index));
-					} else if (isLong) {
-						ih.setInstruction(new PUTSTATIC_LONG((short) index));
-					}
-				} else if (fi instanceof GETFIELD) {
-					if (isRef) {
-						ih.setInstruction(new GETFIELD_REF((short) index));
-					} else if (isLong) {
-						ih.setInstruction(new GETFIELD_LONG((short) index));
-					}
-				} else if (fi instanceof PUTFIELD) {
-					if (isRef) {
-						ih.setInstruction(new PUTFIELD_REF((short) index));
-					} else if (isLong) {
-						ih.setInstruction(new PUTFIELD_LONG((short) index));
-					}
-				}
-
-			}
-		}
-
 		
 		
 		f = new InstructionFinder(il);
@@ -130,26 +89,79 @@ public class ReplaceNativeAndCPIdx extends MyVisitor {
 		String cpInstr = "CPInstruction";		
 		for(Iterator it = f.search(cpInstr); it.hasNext(); ) {
 			InstructionHandle[] match = (InstructionHandle[])it.next();
-			InstructionHandle   first = match[0];
+			InstructionHandle   ih = match[0];
 			
-			CPInstruction ii = (CPInstruction)first.getInstruction();
-			int i = ii.getIndex();
-			Integer idx = new Integer(i);
+			CPInstruction cpii = (CPInstruction)ih.getInstruction();
+			int index = cpii.getIndex();
+			
+			// we have to grab the information before we change
+			// the CP index.
+			FieldInstruction fi = null;
+			Type ft = null;
+			if (cpii instanceof FieldInstruction) {
+				fi = (FieldInstruction) ih.getInstruction();
+				ft = fi.getFieldType(cpool);
+			}
+
+			Integer idx = new Integer(index);
 			// pos is the new position in the reduced constant pool
 			// idx is the position in the 'original' unresolved cpool
 			int pos = cli.cpoolUsed.indexOf(idx);
+			int new_index = pos+1;
 			if (pos==-1) {
-				System.out.println("Error: constant "+i+" "+cpool.getConstant(i)+
+				System.out.println("Error: constant "+index+" "+cpool.getConstant(index)+
 						" not found");
 				System.out.println("new cpool: "+cli.cpoolUsed);
 				System.out.println("original cpool: "+cpool);
 				
 				System.exit(-1);
 			} else {
+//				if (cpii instanceof GETFIELD || cpii instanceof GETFIELD) {
+//					System.out.println("CPI get/putfield");
+//				} else if (cpii instanceof GETFIELD_REF){
+//					System.out.println("CPI getfield_ref");					
+//				}
 				// set new index, position starts at
 				// 1 as cp points to the length of the pool
 // System.out.println(cli.clazz.getClassName()+"."+method.getName()+" "+ii+" -> "+(pos+1));
-				ii.setIndex(pos+1);
+				cpii.setIndex(new_index);
+			}
+			
+			if (cpii instanceof FieldInstruction) {
+//				System.out.println("Field instruction");
+				
+				boolean isRef = ft instanceof ReferenceType;
+				boolean	isLong = ft==BasicType.LONG || ft==BasicType.DOUBLE;
+
+				if (fi instanceof GETSTATIC) {
+					if (isRef) {
+						ih.setInstruction(new GETSTATIC_REF((short) new_index));
+					} else if (isLong) {
+						ih.setInstruction(new GETSTATIC_LONG((short) new_index));
+					}
+				} else if (fi instanceof PUTSTATIC) {
+					if (isRef) {
+						ih.setInstruction(new PUTSTATIC_REF((short) new_index));
+					} else if (isLong) {
+						ih.setInstruction(new PUTSTATIC_LONG((short) new_index));
+					}
+				} else if (fi instanceof GETFIELD) {
+					if (isRef) {
+						ih.setInstruction(new GETFIELD_REF((short) new_index));
+//						System.out.println("get ref");
+					} else if (isLong) {
+						ih.setInstruction(new GETFIELD_LONG((short) new_index));
+//						System.out.println("get long");
+					} else {
+//						System.out.println("get word");						
+					}
+				} else if (fi instanceof PUTFIELD) {
+					if (isRef) {
+						ih.setInstruction(new PUTFIELD_REF((short) new_index));
+					} else if (isLong) {
+						ih.setInstruction(new PUTFIELD_LONG((short) new_index));
+					}
+				}
 			}
 		}
 		
