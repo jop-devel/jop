4f039be1703272041fb14ee128aebc81da761a17
Martin Schoeberl <martin@jopdesign.com>
2006-02-06 23:39:06 +0000

    add RXTX library for JavaDown, remove jcc library

--- java/tools/build.bat
+++ java/tools/build.bat
@@ -3,7 +3,7 @@ mkdir dist\lib
 mkdir dist\classes
 dir /b /s ..\target\src\common\com\jopdesign\sys\Const.java > .sourcefiles
 dir /b /s src\*.java >> .sourcefiles
-set java_lib=../tools/dist/lib/jop-tools.jar;../lib/bcel-5.1.jar;../lib/jakarta-regexp-1.3.jar;jcc.jar
+set java_lib=../tools/dist/lib/jop-tools.jar;../lib/bcel-5.1.jar;../lib/jakarta-regexp-1.3.jar;../lib/RXTXcomm.jar
 javac -g -d dist\classes -classpath %java_lib% @.sourcefiles
 del .sourcefiles
 cd dist\classes
