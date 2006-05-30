dc41a4b1a058e991feb6ac192de9198368127a56
rasped <rasped>
2006-05-30 09:14:19 +0000

    Cache

---
+++ java/tools/src/com/jopdesign/tools/CacheSimul.java
@@ -0,0 +1,8 @@
+package com.jopdesign.tools;
+public class CacheSimul {
+  // return true on a cache hit
+  public static boolean get(String methodid){
+    return false;
+  }
+  public static void clear(){};
+}
