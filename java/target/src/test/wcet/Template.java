78fc6fcae8a311fdef73911aa4c836cd9f3a5249
Martin Schoeberl <martin@jopdesign.com>
2006-06-17 23:34:58 +0000

    no message

---
+++ java/target/src/test/wcet/Template.java
@@ -0,0 +1,33 @@
+package wcet;
+
+import com.jopdesign.sys.*;
+
+public class Template {
+
+	static int ts, te, to;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ts = Native.rdMem(Const.IO_CNT);
+		te = Native.rdMem(Const.IO_CNT);
+		to = te-ts;
+		// measurement + return takes 22+22+21=65 cycles
+		// WCET measured: x
+		// WCET analysed: y
+		measure();
+		System.out.println(te-ts-to);
+	}
+	
+	static void measure() {
+		ts = Native.rdMem(Const.IO_CNT);
+		int val = 123;
+		for (int i=0; i<100; ++i) { // @WCA loop=100
+			val += i;
+		}
+		te = Native.rdMem(Const.IO_CNT);		
+	}
+	
+}
