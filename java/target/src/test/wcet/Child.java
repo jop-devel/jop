a27295ef5fa155801902c4ac9c0c4067ec2940d0
rasped <rasped>
2006-06-24 08:02:20 +0000

    Inheritance example

---
+++ java/target/src/test/wcet/Child.java
@@ -0,0 +1,11 @@
+package wcet;
+
+public class Child extends Parent{
+	public static void main(String[] args){
+    measure();    
+  }
+  
+  public static void measure(){
+    loop();
+  }
+}
---
+++ java/target/src/test/wcet/Parent.java
@@ -0,0 +1,7 @@
+package wcet;
+
+public abstract class Parent {
+	public static void loop() {
+		int k=3;
+	}
+}
