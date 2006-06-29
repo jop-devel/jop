d5392ec4b7b20b49a50db68b5fa50a31bfb08149
rasped <rasped>
2006-06-29 18:00:46 +0000

    no message

---
+++ java/target/src/test/wcet/StartGCStkWlk.java
@@ -0,0 +1,15 @@
+package wcet;
+
+import wcet.gc.GCStkWalk;
+
+public class StartGCStkWlk {
+
+
+	public static void main(String[] args) {
+
+		// initialization
+		GCStkWalk.swk(0,true,true);
+    
+	}
+			
+}
