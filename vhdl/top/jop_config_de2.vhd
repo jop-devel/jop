43a42fce2b072f293ce03230ef2f2e13306a2ef7
Martin Schoeberl <martin@jopdesign.com>
2006-08-11 19:31:56 +0000

    100 MHz with PLL

--- vhdl/top/jop_config_de2.vhd
+++ vhdl/top/jop_config_de2.vhd
@@ -9,9 +9,9 @@ use ieee.std_logic_1164.all;
 
 package jop_config is
 
-	-- constants for 50MHz input and 100MHz internal clock
-	constant clk_freq : integer := 50000000;
-	constant pll_mult : natural := 1;
+	-- constants for 50MHz input clock
+	constant clk_freq : integer := 100000000;
+	constant pll_mult : natural := 2;
 	constant pll_div : natural := 1;
 
 end jop_config;
