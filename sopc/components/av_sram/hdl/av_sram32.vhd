adf17f504251c3a557a2380a8e8932c1be82af54
Martin Schoeberl <martin@jopdesign.com>
2006-08-14 13:53:12 +0000

    SRAM interface with VHDL code

---
+++ sopc/components/av_sram/hdl/av_sram32.vhd
@@ -0,0 +1,299 @@
+--
+--	av_sram32.vhd
+--
+--	Avalon compliant external memory interface
+--	for 32-bit SRAM (e.g. Cyclone board, Spartan-3 Starter Kit)
+--
+--	memory mapping
+--	
+--		000000-x7ffff	external SRAM (w mirror)	max. 512 kW (4*4 MBit)
+--
+--	RAM: 32 bit word
+--
+--
+--	2006-08-13	Adapted from sc_ram32
+--
+
+Library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity av_sram is
+generic (ram_ws : integer; addr_bits : integer);
+
+port (
+
+	clk, reset	: in std_logic;
+
+-- Avalon interface
+
+	address			: in std_logic_vector(addr_bits-1 downto 0);
+	chipselect		: in std_logic; -- do I need it?
+	read, write		: in std_logic;
+	writedata		: in std_logic_vector(31 downto 0);
+	readdata		: out std_logic_vector(31 downto 0);
+	waitrequest		: out std_logic;
+
+-- memory interface
+
+	ram_addr	: out std_logic_vector(addr_bits-1 downto 0);
+	ram_data	: inout std_logic_vector(31 downto 0);
+	ram_ncs		: out std_logic;
+	ram_noe		: out std_logic;
+	ram_nwe		: out std_logic
+
+);
+end av_sram;
+
+architecture rtl of av_sram is
+
+--
+--	signals for mem interface
+--
+	type state_type		is (
+							idl, rd1, rd2,
+							wr1, rdy
+						);
+	signal state 		: state_type;
+	signal next_state	: state_type;
+
+	signal nwr_int		: std_logic;
+	signal wait_state	: unsigned(3 downto 0);
+	signal cnt			: unsigned(1 downto 0);
+
+	signal dout_ena		: std_logic;
+	signal rd_data_ena	: std_logic;
+
+	signal rd, wr		: std_logic;
+
+	signal ram_din		: std_logic_vector(31 downto 0);
+	signal ram_dout		: std_logic_vector(31 downto 0);
+
+begin
+
+	process(dout_ena, ram_dout)
+	begin
+		if dout_ena='1' then
+			ram_data <= ram_dout;
+		else
+			ram_data <= (others => 'Z');
+		end if;
+	end process;
+	ram_din <= ram_data;
+
+--
+--	Register memory address, write data and read data
+--
+process(clk, reset)
+begin
+	if reset='1' then
+
+		ram_addr <= (others => '0');
+		ram_dout <= (others => '0');
+		readdata <= (others => '0');
+
+	elsif rising_edge(clk) then
+
+		if rd='1' or wr='1' then
+			ram_addr <= address;
+		end if;
+		if wr='1' then
+			ram_dout <= writedata;
+		end if;
+		if rd_data_ena='1' then
+			readdata <= ram_din;
+		end if;
+
+	end if;
+end process;
+
+--
+--	'delay' nwe 1/2 cycle -> change on falling edge
+--
+process(clk, reset)
+
+begin
+	if (reset='1') then
+		ram_nwe <= '1';
+	elsif falling_edge(clk) then
+		ram_nwe <= nwr_int;
+	end if;
+
+end process;
+
+process(state, read, write)
+
+begin
+	waitrequest <= '1';
+	if (state=idl and read='0' and write='0') or state=rdy then
+		waitrequest <= '0';
+	end if;
+end process;
+
+--
+--	next state logic
+--
+process(state, read, write, wait_state)
+
+begin
+
+	next_state <= state;
+
+	rd <= '0';
+	wr <= '0';
+
+	case state is
+
+		when idl =>
+			if chipselect='1' then
+				if read='1' then
+					rd <= '1';
+					if ram_ws=0 then
+						-- then we omit state rd1!
+						next_state <= rd2;
+					else
+						next_state <= rd1;
+					end if;
+				elsif write='1' then
+					wr <= '1';
+					next_state <= wr1;
+				end if;
+			end if;
+
+		-- the WS state
+		when rd1 =>
+			if wait_state=2 then
+				next_state <= rd2;
+			end if;
+
+		-- last read state
+		when rd2 =>
+			next_state <= rdy;
+-- avoid this on Avalon - read/write is set until waitrequest goes
+-- to 0.
+--			-- This should do to give us a pipeline
+--			-- level of 2 for read
+--			if rd='1' then
+--				if ram_ws=0 then
+--					-- then we omit state rd1!
+--					next_state <= rd2;
+--				else
+--					next_state <= rd1;
+--				end if;
+--			elsif wr='1' then
+--				next_state <= wr1;
+--			end if;
+			
+		-- the WS state
+		when wr1 =>
+			if wait_state=1 then
+				next_state <= rdy;
+			end if;
+
+		-- one extra state as read/write is still active
+		-- on the last cycle where we have to deassert
+		-- waitrequest
+		when rdy =>
+			next_state <= idl;
+
+	end case;
+				
+end process;
+
+--
+--	state machine register
+--	output register
+--
+process(clk, reset)
+
+begin
+	if (reset='1') then
+		state <= idl;
+		dout_ena <= '0';
+		ram_ncs <= '1';
+		ram_noe <= '1';
+		rd_data_ena <= '0';
+	elsif rising_edge(clk) then
+
+		state <= next_state;
+		dout_ena <= '0';
+		ram_ncs <= '1';
+		ram_noe <= '1';
+		rd_data_ena <= '0';
+
+		case next_state is
+
+			when idl =>
+
+			-- the wait state
+			when rd1 =>
+				ram_ncs <= '0';
+				ram_noe <= '0';
+
+			-- last read state
+			when rd2 =>
+				ram_ncs <= '0';
+				ram_noe <= '0';
+				rd_data_ena <= '1';
+				
+				
+			-- the WS state
+			when wr1 =>
+				ram_ncs <= '0';
+				dout_ena <= '1';
+
+			when rdy =>
+
+		end case;
+					
+	end if;
+end process;
+
+--
+--	nwr combinatorial processing
+--	for the negativ edge
+--
+process(next_state, state)
+begin
+
+	nwr_int <= '1';
+	if next_state=wr1 then
+		nwr_int <= '0';
+	end if;
+
+end process;
+
+--
+-- wait_state processing
+-- cs delay, dout enable
+--
+process(clk, reset)
+begin
+	if (reset='1') then
+		wait_state <= (others => '1');
+		cnt <= "00";
+	elsif rising_edge(clk) then
+
+		wait_state <= wait_state-1;
+
+		cnt <= "11";
+		if next_state=idl then
+			cnt <= "00";
+		-- if wait_state<4 then
+		elsif wait_state(3 downto 2)="00" then
+			cnt <= wait_state(1 downto 0)-1;
+		end if;
+
+		if rd='1' or wr='1' then
+			wait_state <= to_unsigned(ram_ws+1, 4);
+			if ram_ws<3 then
+				cnt <= to_unsigned(ram_ws+1, 2);
+			else
+				cnt <= "11";
+			end if;
+		end if;
+
+	end if;
+end process;
+
+end rtl;
---
+++ sopc/toplevel/cycore_jop.vhd
@@ -0,0 +1,150 @@
+--
+--	cycore_jop.vhd
+--
+--	Author: Martin Schoeberl (martin@jopdesign.com)
+--
+--	top level for SOPC/JOP experiments
+--
+--	Just the minimum version with a 256x32 SRAM, boot UART,
+--	and a watchdog LED
+--
+--	2006-08-10	created from jopcyc.vhd
+--
+--
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.jop_config.all;
+
+entity cycore_jop is
+
+port (
+	clk			: in std_logic;
+
+	--
+	--	serial interface to download the
+	--	Java application and System.in/out
+	--
+	ser_rxd		: in std_logic;
+	ser_txd		: out std_logic;
+
+	--
+	--	two ram banks
+	--
+	rama_a		: out std_logic_vector(17 downto 0);
+	rama_d		: inout std_logic_vector(15 downto 0);
+	rama_ncs	: out std_logic;
+	rama_noe	: out std_logic;
+	rama_nlb	: out std_logic;
+	rama_nub	: out std_logic;
+	rama_nwe	: out std_logic;
+	ramb_a		: out std_logic_vector(17 downto 0);
+	ramb_d		: inout std_logic_vector(15 downto 0);
+	ramb_ncs	: out std_logic;
+	ramb_noe	: out std_logic;
+	ramb_nlb	: out std_logic;
+	ramb_nub	: out std_logic;
+	ramb_nwe	: out std_logic;
+
+	-- watchdog LED
+	wd			: out std_logic
+);
+end cycore_jop;
+
+architecture rtl of cycore_jop is
+
+	component pll is
+	generic (multiply_by : natural; divide_by : natural);
+	port (
+		inclk0		: in std_logic;
+		c0			: out std_logic
+	);
+	end component;
+
+	signal clk_int			: std_logic;
+
+	signal int_res			: std_logic;
+	signal res_cnt			: unsigned(2 downto 0) := "000";	-- for the simulation
+
+	-- for generation of internal reset
+	attribute altera_attribute : string;
+	attribute altera_attribute of res_cnt : signal is "POWER_UP_LEVEL=LOW";
+
+	signal byte_nena		: std_logic_vector(3 downto 0);
+	signal address			: std_logic_vector(17 downto 0);
+
+	signal ncs, noe, nwe	: std_logic; 
+
+	signal reset_n			: std_logic;
+
+begin
+
+	pll_inst : pll generic map(
+		multiply_by => pll_mult,
+		divide_by => pll_div
+	)
+	port map (
+		inclk0	 => clk,
+		c0	 => clk_int
+	);
+-- if you don't like the PLL use this:
+-- clk_int <= clk;
+
+	--
+	--	internal reset
+	--	no external reset needed
+	--
+	process(clk_int)
+	begin
+		if rising_edge(clk_int) then
+			if (res_cnt/="111") then
+				res_cnt <= res_cnt+1;
+			end if;
+
+			int_res <= not res_cnt(0) or not res_cnt(1) or not res_cnt(2);
+		end if;
+	end process;
+
+	reset_n <= not int_res;
+
+	--	the SOPC generated top level
+	jop: work.jop_system port map (
+		clk => clk_int,
+		reset_n => reset_n,
+		ser_rxd_to_the_jop_avalon_0 => ser_rxd,
+		ser_txd_from_the_jop_avalon_0 => ser_txd,
+		wd_from_the_jop_avalon_0 => wd,
+
+		-- the_av_sram_0
+		ram_addr_from_the_av_sram_0 => address,
+		ram_data_to_and_from_the_av_sram_0(31 downto 16) => ramb_d,
+		ram_data_to_and_from_the_av_sram_0(15 downto 0) => rama_d,
+		ram_ncs_from_the_av_sram_0 => ncs,
+		ram_noe_from_the_av_sram_0 => noe,
+		ram_nwe_from_the_av_sram_0 => nwe
+	);
+	
+	rama_nlb <= '0';
+	rama_nub <= '0';
+	ramb_nlb <= '0';
+	ramb_nub <= '0';
+
+	rama_ncs <= ncs;
+	rama_noe <= noe;
+	rama_nwe <= nwe;
+	ramb_ncs <= ncs;
+	ramb_noe <= noe;
+	ramb_nwe <= nwe;
+
+	-- A0/1 from the avalon interface is NC on 32-bit SRAM
+--	rama_a <= address(19 downto 2);
+--	ramb_a <= address(19 downto 2);
+
+	-- not for the Avalon slave version without tri-state brigde
+	rama_a <= address;
+	ramb_a <= address;
+
+end rtl;
