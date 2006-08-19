da5f0995b8c817492d4f5961320023d2e1b660da
Martin Schoeberl <martin@jopdesign.com>
2006-08-19 13:02:39 +0000

    no message

---
+++ java/target/src/test/jdk/HashCode.java
@@ -0,0 +1,17 @@
+package jdk;
+
+public class HashCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(new Object().hashCode());
+		System.out.println(new Object().hashCode());
+		System.out.println(new Object().hashCode());
+
+	}
+
+}
