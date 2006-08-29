4c425a2fb7116dcf4083a4db33b3ee7f7675eb32
Martin Schoeberl <martin@jopdesign.com>
2006-08-29 20:03:31 +0000

    FUCA test example

--- java/target/src/test/wcet/Method.java
+++ java/target/src/test/wcet/Method.java
@@ -6,11 +6,12 @@ public class Method {
 
 	static int ts, te, to;
 
+	static boolean dummy = true;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		Method m = new Method();
 		ts = Native.rdMem(Const.IO_CNT);
 		te = Native.rdMem(Const.IO_CNT);
@@ -19,21 +20,50 @@ public class Method {
 		// WCET with two block cache: 1116
 		// WCET analysed: 1312
 		measure();
-//		System.out.println(te-ts-to);
+		System.out.println(te-ts-to);
 	}
-	
+
 	static void measure() {
 		ts = Native.rdMem(Const.IO_CNT);
-		y();
-		te = Native.rdMem(Const.IO_CNT);		
+		for (int i=0; i<10; ++i) { // @WCA loop=10
+            if (dummy) {
+                b();
+            } else {
+                d();
+            }
+		}
+		te = Native.rdMem(Const.IO_CNT);
 	}
-	
-	static void y() {
-		for (int i=0; i<5; ++i) { // @WCA loop=5
-			x();		
-		}		
+
+	static void b() {
+		int i;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		for (i=0; i<5; ++i) { // @WCA loop=5
+			c();
+		}
 	}
-	static void x() {
+	static void c() {
 
 		int a, b, c, d, e;
 		int i = 123;
@@ -53,6 +83,47 @@ public class Method {
 		i = 456;
 		i = 456;
 		i = 456;
-		
+
+	}
+	static void d() {
+
+		int a, b, c, d, e;
+		int i = 123;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+		i = 456;
+
 	}
 }
