b3792cabffd44f22a6084c4f8b282e3312d30624
Martin Schoeberl <martin@jopdesign.com>
2006-10-19 10:00:19 +0000

    not used

--- java/pc/build.bat
+++
@@ -1,9 +0,0 @@
-rmdir /Q /S dist
-mkdir dist\lib
-mkdir dist\classes
-dir /b /s src\*.java > .sourcefiles
-javac -g -d dist\classes -classpath jcc.jar -sourcepath .;../target/src/common @.sourcefiles
-del .sourcefiles
-cd dist\classes
-jar cf ..\lib\jop-pc.jar *
-cd ..\..
