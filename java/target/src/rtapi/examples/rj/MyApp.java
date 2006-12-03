41c133822a663cc71cdeb9a88fb1c755ccf2b493
Martin Schoeberl <martin@jopdesign.com>
2006-12-03 17:51:25 +0000

    ravenscar examples

---
+++ java/target/src/rtapi/examples/rj/MyApp.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 22.10.2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package examples.rj;
+
+import javax.realtime.AbsoluteTime;
+import javax.realtime.RelativeTime;
+import ravenscar.*;
+
+
+/**
+ * @author martin
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class MyApp extends Initializer {
+
+	public void run() {
+		// init
+		new PeriodicThread(
+			new PriorityParameters(10),
+			new PeriodicParameters(
+				new AbsoluteTime(0, 0),
+				new RelativeTime(100, 0)
+			),
+			new Runnable() {
+				public void run() {
+					System.out.print("*");
+				}
+			}
+		);
+	}
+	public static void main(String[] args) {
+
+		MyApp ma = new MyApp();
+		ma.start();
+	}
+}
---
+++ java/target/src/rtapi/examples/rj/MyEvent.java
@@ -0,0 +1,74 @@
+/*
+ * Created on 22.10.2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package examples.rj;
+
+import javax.realtime.AbsoluteTime;
+import javax.realtime.RelativeTime;
+import ravenscar.*;
+
+
+
+/**
+ * @author martin
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class MyEvent extends Initializer {
+
+	public void run() {
+
+		// init
+
+		//
+		//	an event handler
+		//
+		SporadicEventHandler evh = new SporadicEventHandler(
+			new PriorityParameters(15),		// higher then periodic thread
+			new SporadicParameters(
+				new RelativeTime(33, 0),
+				1							// what means buffer size?
+			))
+		{
+			public void handleAsyncEvent() {
+
+				System.out.print("fire!");
+			}
+		};
+
+		//
+		//	an sw event
+		//
+		final SporadicEvent ev = new SporadicEvent(evh);
+
+
+		//
+		//	one periodic Thread
+		//
+		new PeriodicThread(
+			new PriorityParameters(10),
+			new PeriodicParameters(
+				new AbsoluteTime(0, 0),
+				new RelativeTime(1000, 0)
+			),
+			new Runnable() {
+				public void run() {
+					System.out.print("befor ");
+					ev.fire();
+					System.out.println(" after");
+				}
+			}
+		);
+	}
+
+
+	public static void main(String[] args) {
+
+		MyEvent ma = new MyEvent();
+		ma.start();
+	}
+}
