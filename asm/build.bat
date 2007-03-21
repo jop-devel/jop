d55079d8f848728283f4f788bc10a2be3f669a4f
Martin Schoeberl <martin@jopdesign.com>
2007-03-21 18:34:50 +0000

    add generation for 16K blocks for Virtex4/5

--- asm/build.bat
+++ asm/build.bat
@@ -6,6 +6,7 @@ rem mkdir generated
 java -cp ..\java\tools\dist\lib\jop-tools.jar com.jopdesign.tools.Jopa -s src -d generated %project%.asm
 rem java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -d 1024 -w 8 -m xjbc_block -o generated
 java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -m xram_block generated\ram.mif generated\xram_block.vhd
+java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b 16384 -pd -m xram_block generated\ram.mif generated\xv4ram_block.vhd
 rem java -cp ..\java\tools\dist\lib\jop-tools.jar BlockGen -b %blocksize% -pd -m xrom_block generated\rom.mif generated\xrom_block.vhd
 copy generated\*.vhd ..\vhdl
 
