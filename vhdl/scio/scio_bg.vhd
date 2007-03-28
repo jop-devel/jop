e2d2da2727ba101dff0aac7d5a4819755f674545
Martin Schoeberl <martin@jopdesign.com>
2007-03-28 12:35:19 +0000

    SimpCon with records

--- vhdl/scio/scio_bg.vhd
+++ vhdl/scio/scio_bg.vhd
@@ -30,6 +30,7 @@
 --	2005-08-27	ignore ncts on uart
 --	2005-12-27	change for SimpCon, HW hand shake is programmable
 --				for uart_tal
+--	2007-03-28	use records
 --
 
 
@@ -391,32 +392,26 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.jop_types.all;
+use work.sc_pack.all;
 use work.jop_config.all;
 
 entity scio is
-generic (addr_bits : integer);
 
 port (
 	clk		: in std_logic;
 	reset	: in std_logic;
 
--- SimpCon interface
+--
+--	SimpCon IO interface
+--
+	sc_io_out		: in sc_io_out_type;
+	sc_io_in		: out sc_in_type;
 
-	address		: in std_logic_vector(addr_bits-1 downto 0);
-	wr_data		: in std_logic_vector(31 downto 0);
-	rd, wr		: in std_logic;
-	rd_data		: out std_logic_vector(31 downto 0);
-	rdy_cnt		: out unsigned(1 downto 0);
-
--- interrupt
-
-	irq			: out std_logic;
-	irq_ena		: out std_logic;
-
--- exception
-
-	exc_req		: in exception_type;
-	exc_int		: out std_logic;
+--
+--	Interrupts from IO devices
+--
+	irq_in			: out irq_in_type;
+	exc_req			: in exception_type;
 
 -- serial interface
 
@@ -481,22 +476,19 @@ begin
 
 	assert SLAVE_CNT <= 2**DECODE_BITS report "Wrong constant in scio";
 
-	sel <= to_integer(unsigned(address(SLAVE_ADDR_BITS+DECODE_BITS-1 downto SLAVE_ADDR_BITS)));
+	sel <= to_integer(unsigned(sc_io_out.address(SLAVE_ADDR_BITS+DECODE_BITS-1 downto SLAVE_ADDR_BITS)));
 
 	-- What happens when sel_reg > SLAVE_CNT-1??
-	rd_data <= sc_dout(sel_reg);
-
-	--	rdy_cnt <= sc_rdy_cnt(sel_reg);
-	-- all IO devices are single cycle
-	rdy_cnt <= "00";
+	sc_io_in.rd_data <= sc_dout(sel_reg);
+	sc_io_in.rdy_cnt <= sc_rdy_cnt(sel_reg);
 
 	--
 	-- Connect SLAVE_CNT slaves
 	--
 	gsl: for i in 0 to SLAVE_CNT-1 generate
 
-		sc_rd(i) <= rd when i=sel else '0';
-		sc_wr(i) <= wr when i=sel else '0';
+		sc_rd(i) <= sc_io_out.rd when i=sel else '0';
+		sc_wr(i) <= sc_io_out.wr when i=sel else '0';
 
 	end generate;
 
@@ -508,7 +500,7 @@ begin
 		if (reset='1') then
 			sel_reg <= 0;
 		elsif rising_edge(clk) then
-			if rd='1' then
+			if sc_io_out.rd='1' then
 				sel_reg <= sel;
 			end if;
 		end if;
@@ -522,18 +514,15 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(0),
 			wr => sc_wr(0),
 			rd_data => sc_dout(0),
 			rdy_cnt => sc_rdy_cnt(0),
 
-			irq => irq,
-			irq_ena => irq_ena,
-
+			irq_in => irq_in,
 			exc_req => exc_req,
-			exc_int => exc_int,
 			
 			wd => wd
 		);
@@ -552,8 +541,8 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(1),
 			wr => sc_wr(1),
 			rd_data => sc_dout(1),
@@ -566,6 +555,8 @@ begin
 	);
 
 	-- slave 2 is reserved for USB and System.out writes to it!!!
+	sc_rdy_cnt(2) <= (others => '0');
+	sc_dout(2) <= (others => '0');
 
 	-- Siemens TC35 sends up to 32!!! characters after cts deasert
 	-- WW does not like the 115200 baud :-(
@@ -582,8 +573,8 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(3),
 			wr => sc_wr(3),
 			rd_data => sc_dout(3),
@@ -609,8 +600,8 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(4),
 			wr => sc_wr(4),
 			rd_data => sc_dout(4),
@@ -633,8 +624,8 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(6),
 			wr => sc_wr(6),
 			rd_data => sc_dout(6),
@@ -661,8 +652,8 @@ begin
 			clk => clk,
 			reset => reset,
 
-			address => address(SLAVE_ADDR_BITS-1 downto 0),
-			wr_data => wr_data,
+			address => sc_io_out.address(SLAVE_ADDR_BITS-1 downto 0),
+			wr_data => sc_io_out.wr_data,
 			rd => sc_rd(5),
 			wr => sc_wr(5),
 			rd_data => sc_dout(5),
