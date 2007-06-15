de9e586639a67848639aec01a10b45c91df2482c
9914pich <9914pich>
2007-06-15 18:28:48 +0000

    no message

---
+++ java/target/src/bench/jbe/BenchCMP2.java
@@ -0,0 +1,48 @@
+package jbe;
+
+import com.jopdesign.sys.Const;
+import com.jopdesign.sys.Native;
+
+// Benchmark of CMP with 2 JOPs
+
+public class BenchCMP2 {
+
+	public static boolean signal = false; 
+	public static int lift0 = 0;
+	public static int lift1 = 0;	
+	public static BenchMark bm0;
+	public static BenchMark bm1;
+
+	public static void main(String[] args) {
+
+		int cpu_id;
+		cpu_id = Native.rdMem(Const.IO_CPU_ID);
+		
+		if (cpu_id == 0x00000000)
+		{
+			bm0 = new BenchLift();
+			bm1 = new BenchLift();
+			
+			Native.wrMem(0x00000001, Const.IO_SIGNAL);
+				
+			LowLevel.msg("Application benchmarks:");
+			LowLevel.lf();
+			
+			lift0 = Execute.perform(bm0);
+			while(signal == false);
+			
+			LowLevel.msg("Lift on JOP0:", lift0);
+			LowLevel.lf();
+			LowLevel.msg("Lift on JOP1:", lift1);
+			LowLevel.lf();	
+		}
+		else
+		{		
+			if (cpu_id == 0x00000001)
+			{	
+				lift1 = Execute.perform(bm1);
+				signal = true;
+			}
+		}
+	}		
+}
--- java/target/src/bench/jbe/BenchCMP3.java
+++ java/target/src/bench/jbe/BenchCMP3.java
@@ -3,15 +3,17 @@ package jbe;
 import com.jopdesign.sys.Const;
 import com.jopdesign.sys.Native;
 
-// Test of CMP with 2 JOPs
+// Benchmark of CMP with 3 JOPs
 
-public class BenchCMP2 {
+public class BenchCMP3 {
 
-	public static boolean signal = false; 
+	public static int signal = 0; 
 	public static int lift0 = 0;
 	public static int lift1 = 0;	
+	public static int lift2 = 0;	
 	public static BenchMark bm0;
 	public static BenchMark bm1;
+	public static BenchMark bm2;
 
 
 	public static void main(String[] args) {
@@ -23,25 +25,34 @@ public class BenchCMP2 {
 		{
 			bm0 = new BenchLift();
 			bm1 = new BenchLift();
+			bm2 = new BenchLift();
+
+			Native.wrMem(0x00000001, Const.IO_SIGNAL);		
 			
-			Native.wrMem(0x00000001, Const.IO_SIGNAL);
-				
 			LowLevel.msg("Application benchmarks:");
 			LowLevel.lf();
-			
 			lift0 = Execute.perform(bm0);
-			while(signal == false);
+			while(signal != 2);
 			
 			LowLevel.msg("Lift on JOP0:", lift0);
 			LowLevel.lf();
 			LowLevel.msg("Lift on JOP1:", lift1);
 			LowLevel.lf();
-			
+			LowLevel.msg("Lift on JOP2:", lift2);
+			LowLevel.lf();
 		}
 		else
-		{			
-			lift1 = Execute.perform(bm1);
-			signal = true;
+		{	
+			if (cpu_id == 0x00000001)
+			{	
+				lift1 = Execute.perform(bm1);
+				signal++;
+			}
+			else
+			{
+				lift2 = Execute.perform(bm2);
+				signal++;
+			}
 		}
-	}		
+	}
 }
