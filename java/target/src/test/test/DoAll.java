1a4be0f0e9637e2e31dd09a2f63fd3d953a6c50c
Martin Schoeberl <martin@jopdesign.com>
2007-08-04 16:36:55 +0000

    no message

---
+++ java/target/src/test/test/DoAll.java
@@ -0,0 +1,15 @@
+package test;
+
+public class DoAll {
+
+	/**
+	 * Invoke all tests we have so far
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		jvm.DoAll.main(args);
+		jdk.DoAll.main(args);
+	}
+
+}
