676a52db7b0fe69d1d881c74990aebe6dc945da5
Martin Schoeberl <martin@jopdesign.com>
2007-10-06 23:41:12 +0000

    test for clinit order

--- java/target/src/test/jvm/obj/Clinit2.java
+++ java/target/src/test/jvm/obj/Clinit2.java
@@ -13,12 +13,17 @@ public class Clinit2 extends TestCase {
 
 	
 	static int abc = 123;
+	static int invsta = 0;
 
 	static {
 		abc = 456;
-		System.out.println("clinit2");
 		// the X static initializer runns later
-		new X();
+		// This issue is not solvable with a predefined <clinit> order!
+		// we do not allow to pass in JOPizer
+		// new Clinit2_X();
+		
+		// <clinit> Clinit2_invsta should run before
+		invsta = Clinit2_invstat.getS();
 	}
 	
 	public String getName() {
@@ -28,9 +33,16 @@ public class Clinit2 extends TestCase {
 	public boolean test() {		
 
 		boolean ok = true;
-		if (abc!=789) ok = false;
-
-
+		
+		// we cannot resolve this issue!
+		// if (abc!=789) ok = false;
+		
+		// but that should be possible
+		if (Clinit2_X.result_y!=2) ok = false;
+		if (Clinit2_X.result_z!=4) ok = false;
+		if (Clinit2_invstat.getS()!=4711) ok = false;
+		if (invsta!=4711) ok = false;
+		
 		return ok;
 
 	}
@@ -38,7 +50,10 @@ public class Clinit2 extends TestCase {
 	public static void main(String[] args) {
 		
 		Clinit2 clinit = new Clinit2();
-		new X();
+		// force our transitive hull generater to put
+		// those <clinit> in the wrong order
+		// ... not really possible as it is a HashSet
+		new Clinit2_X();
 		
 		System.out.print(clinit.getName());
 		if (clinit.test()) {
@@ -49,10 +64,45 @@ public class Clinit2 extends TestCase {
 	}
 }
 
-class X {
+class Clinit2_X {
+	
+	static int result_y = 0;
+	static int result_z = 0;
 	
 	static {
 		Clinit2.abc = 789;
-		System.out.println("X");
+		// Y <clinit> should run before
+		result_y = Clinit2_Y.y;
+		// Z <clinit> should run before
+		new Clinit2_Z();
+		result_z = Clinit2_Z.z;
+	}
+}
+
+class Clinit2_Y {
+	
+	static int y = 1;
+	static {
+		y = 2;
+	}
+}
+
+class Clinit2_Z {
+
+	static int z = 3;
+	static {
+		z = 4;
+	}
+
+}
+
+class Clinit2_invstat {
+	
+	static int s = 0;
+	static {
+		s = 4711;
+	}
+	static int getS() {
+		return s;
 	}
 }
