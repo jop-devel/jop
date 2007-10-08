58a04e9a62ce86d1c9e40f59733a111434ee60cc
Martin Schoeberl <martin@jopdesign.com>
2007-10-08 17:32:19 +0000

    moved long table to it's own class to void mix of complext clinit instructions within a long (interpreted) method

--- java/target/src/common/yaffs2/port/yaffs_tagscompat_C.java
+++ java/target/src/common/yaffs2/port/yaffs_tagscompat_C.java
@@ -4,6 +4,10 @@ import yaffs2.utils.*;
 import yaffs2.utils.factory.PrimitiveWrapperFactory;
 
 public class yaffs_tagscompat_C {
+	
+//	static {
+//		System.out.println("clinit tasgcomp_C");
+//	}
 	/*
 	 * YAFFS: Yet Another Flash File System. A NAND-flash specific file system.
 	 *
@@ -32,29 +36,10 @@ public class yaffs_tagscompat_C {
 //	static void yaffs_HandleWriteChunkError(yaffs_Device * dev, int chunkInNAND);
 //	#endif
 
-	static final byte[] yaffs_countBitsTable/*[256]*/ = {
-		0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
-		1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-		1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-		1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-		2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
-		3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-		3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
-		4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
-	};
-
 	static int yaffs_CountBits(/*__u8*/ byte x)
 	{
 		int retVal;
-		retVal = yaffs_countBitsTable[x & 0xff];
+		retVal = yaffs_tagscompat_C_bitsTable.yaffs_countBitsTable[x & 0xff];
 		return retVal;
 	}
 
