f71801b80c23fd07d92cb00864023a52a686b656
peter.hilber <peter.hilber>
2007-11-15 14:27:31 +0000

    *** empty log message ***

---
+++ java/target/src/common/yaffs2/JOPDirectInterfaceClient.java
@@ -0,0 +1,17 @@
+package yaffs2;
+
+import yaffs2.port.Dtest_C;
+
+public class JOPDirectInterfaceClient {
+	public static void main(String[] args)
+	{
+		yaffs2.utils.Globals.startUp(
+				new yaffs2.platform.jop.Simulation_yaffscfg2k_C(),
+				new yaffs2.platform.jop.PortConfiguration(), 
+				null);				
+
+		yaffs2.utils.Globals.configuration.yaffs_StartUp();
+		
+		Dtest_C.small_overwrite_test("/flash/boot",1);
+	}
+}
