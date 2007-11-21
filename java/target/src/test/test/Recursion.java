7550ebe82449b11b435ebdd78833a0d283ef69fa
Martin Schoeberl <martin@jopdesign.com>
2007-11-21 20:01:19 +0000

    plus exception tests

--- java/target/src/test/test/Recursion.java
+++ java/target/src/test/test/Recursion.java
@@ -4,6 +4,18 @@ public class Recursion {
 
 	static int cnt;
 	
+	static void npexcp() {
+		
+		Object o = null;
+		o.hashCode();
+	}
+	
+	static void abexcp() {
+		
+		int[] ia = new int[3];
+		ia[4] = 2;
+	}
+	
 	static void foo() {
 		++cnt;
 		System.out.println("depth= "+cnt);
