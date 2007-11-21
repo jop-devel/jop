5a76bc63e98f4c0b11c00a364c7f6300ea97010a
9914pich <9914pich>
2007-11-21 09:15:59 +0000

    no message

--- vhdl/top/jopmul.vhd
+++ vhdl/top/jopmul.vhd
@@ -37,7 +37,7 @@ generic (
 	rom_cnt		: integer := 15;	-- clock cycles for external rom for 100 MHz
 	jpc_width	: integer := 12;	-- address bits of java bytecode pc = cache size
 	block_bits	: integer := 4;		-- 2*block_bits is number of cache blocks
-	cpu_cnt		: integer := 2		-- number of cpus
+	cpu_cnt		: integer := 3		-- number of cpus
 );
 
 port (
