57225eaac152c7c9dd9ba2d4ae4088976879105a
Martin Schoeberl <martin@jopdesign.com>
2007-12-07 18:27:48 +0000

    IH example

---
+++ java/target/src/common/com/jopdesign/io/examples/InterruptHandler.java
@@ -0,0 +1,35 @@
+package com.jopdesign.io.examples;
+
+import util.Timer;
+
+import com.jopdesign.io.*;
+import com.jopdesign.sys.*;
+
+public class InterruptHandler implements Runnable {
+
+	public static void main(String[] args) {
+
+		IOFactory fact = IOFactory.getFactory();		
+		SerialPort sp = fact.getSerialPort();
+
+		InterruptHandler ih = new InterruptHandler();
+		fact.registerInterruptHandler(1, ih);
+		
+		
+		// should be a function in some system class to
+		// set individual enables and remember the mask
+		Native.wr(0x02, Const.IO_INTMASK);
+
+		for (int i=0; i<20; ++i) {
+			Timer.wd();
+			int t = Timer.getTimeoutMs(200);
+			while (!Timer.timeout(t));
+			Native.wr(1, Const.IO_SWINT);
+		}
+	}
+
+	
+	public void run() {
+		System.out.println("Interrupt fired!");
+	}
+}
