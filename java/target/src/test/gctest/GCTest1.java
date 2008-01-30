6f251c3b813c2858bacf1d837c69f298b44cea54
Martin Schoeberl <martin@jopdesign.com>
2008-01-30 13:07:35 +0000

    changed to single thread test

--- java/target/src/test/gctest/GCTest1.java
+++ java/target/src/test/gctest/GCTest1.java
@@ -1,50 +1,43 @@
 package gctest;
 
-import joprt.RtThread;
 
-import com.jopdesign.sys.Const;
-import com.jopdesign.sys.GC;
-import com.jopdesign.sys.Native;
-
-// GCTest1
-// Tests if GC collects the garbage object. The GC is explicitly invoked.
-
+/**
+ * Tests if GC collects the garbage object. The GC is explicitly invoked.
+ */
 public class GCTest1 {
+	
+	static Runtime rt = Runtime.getRuntime();
 
 	public static void main(String s[]) {
-		new RtThread(20, 1000000) {
-			public void run() {
-				String allocStr = "Allocation problem!";
-				String refStr = "Reference problem";
 
-				Garbage garbage = new Garbage();
-				int prevId = garbage.id;
-				GC.gc();
-				int freeHeap = GC.freeMemory();
-				for (;;) {
-					if (freeHeap != GC.freeMemory()) {
-						System.out.println(allocStr);
-					}
-					garbage = new Garbage();
+		System.out.print("Total memory: ");
+		System.out.println((int) rt.totalMemory());
+		System.out.print("Free memory: ");
+		System.out.println((int) rt.freeMemory());
+		
+		String allocStr = "Allocation problem!";
+		String refStr = "Reference problem";
 
-					if ((garbage.id - prevId) != 1) {
-						System.out.println(refStr);
-						System.exit(-1);
-					}
-					prevId = garbage.id;
-
-					GC.gc(); //Remove the old garbage object
-
-					waitForNextPeriod();
-				}
+		Garbage garbage = new Garbage();
+		int prevId = garbage.id;
+		System.gc();
+		int freeHeap = (int) rt.freeMemory();
+		for (int i=0; i<10; ++i) {
+			if (freeHeap != (int) rt.freeMemory()) {
+				System.out.println(allocStr);
 			}
-		};
+			garbage = new Garbage();
 
-		RtThread.startMission();
+			if ((garbage.id - prevId) != 1) {
+				System.out.println(refStr);
+				System.exit(-1);
+			}
+			prevId = garbage.id;
 
-		for (int i = 0; i < 10; ++i) {
-			RtThread.sleepMs(1000);
+			System.gc(); //Remove the old garbage object
+
 		}
+
 		System.out.println("Test 1 OK");
 	}
 }
