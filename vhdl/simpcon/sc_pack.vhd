f3e9ac2c5c8dbba1f7c925cfd819b3790962611a
Martin Schoeberl <martin@jopdesign.com>
2008-02-20 14:35:22 +0000

    Cleanup of SimpCon types

--- vhdl/simpcon/sc_pack.vhd
+++ vhdl/simpcon/sc_pack.vhd
@@ -16,7 +16,11 @@ use ieee.numeric_std.all;
 
 package sc_pack is
 
-	constant SC_ADDR_SIZE : integer := 21;
+	-- two more bits than needed for the main memory
+	--    one to distinguishe between memory and IO access
+	--    one more to allow memory mirroring for size auto
+	--        detection at boot time
+	constant SC_ADDR_SIZE : integer := 23;
 	constant RDY_CNT_SIZE : integer := 2;
 
 	type sc_out_type is record
