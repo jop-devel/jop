3b97808216d0b454dd70f91a6d308185570ff18c
Martin Schoeberl <martin@jopdesign.com>
2008-02-24 23:07:51 +0000

    license corrected

--- java/target/src/common/com/jopdesign/sys/SoftFloat.java
+++ java/target/src/common/com/jopdesign/sys/SoftFloat.java
@@ -1,26 +1,8 @@
-/*
-  This file is part of JOP, the Java Optimized Processor
-    see <http://www.jopdesign.com/>
-
-  Copyright (C) 2001-2008, Martin Schoeberl (martin@jopdesign.com)
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 /* This Java source file is derived from the SoftFloat IEC/IEEE
    Floating-point Arithmetic Package, Release 2b, which requires the
    following paragraphs to be included: */
+
 /*============================================================================
 
 This C source file is part of the SoftFloat IEC/IEEE Floating-point Arithmetic
@@ -51,6 +33,10 @@ the work is derivative, and (2) the source code includes prominent notice with
 these four paragraphs for those parts of this code that are retained.
 
 =============================================================================*/
+
+/*
+  Adapted to Java (2005, 2007) by Martin Schoeberl (martin@jopdesign.com)
+*/
 
 package com.jopdesign.sys;
 /**
