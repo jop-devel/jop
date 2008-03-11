60459c7252a435313d8631815ba2bbade3fa03b2
Martin Schoeberl <martin@jopdesign.com>
2008-03-11 19:27:38 +0000

    Run a benchmark for a constant 10000 iterations

---
+++ java/target/src/bench/jbe/RunBench.java
@@ -0,0 +1,59 @@
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
+
+  Copyright (C) 2005-2008, Martin Schoeberl (martin@jopdesign.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/**
+ * 
+ */
+package jbe;
+
+/**
+ * Run a single benchmark for a constant iteration count.
+ * @author martin
+ *
+ */
+public class RunBench {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		BenchMark bm = new BenchKfl();
+		
+		// remove the following to avoid initializing the JDK
+		System.out.print("Banchmerk ");
+		System.out.print(bm.getName());
+		System.out.println(" started");
+		
+		// start measurement here
+//		int t1 = (int) System.currentTimeMillis();
+//		int cyc1 = com.jopdesign.sys.Native.rdMem(com.jopdesign.sys.Const.IO_CNT);
+		bm.test(10000);
+		// stop measurement here
+//		int cyc2 = com.jopdesign.sys.Native.rdMem(com.jopdesign.sys.Const.IO_CNT);
+//		int t2 = (int) System.currentTimeMillis();
+//		System.out.print("cycles: ");
+//		System.out.println(cyc2-cyc1);
+//		System.out.print("ms: ");
+//		System.out.println(t2-t1);
+	}
+
+}
