a24d615b1ecc7479bdc446b5e3ffc9b1d9d2da17
Martin Schoeberl <martin@jopdesign.com>
2008-04-08 11:24:10 +0000

    Serial HWO example

---
+++ java/target/src/common/com/jopdesign/io/examples/HelloSerial.java
@@ -0,0 +1,53 @@
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
+
+  Copyright (C) 2001-2008, Martin Schoeberl (martin@jopdesign.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.jopdesign.io.examples;
+
+import util.Timer;
+
+import com.jopdesign.io.*;
+
+public class HelloSerial {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+		IOFactory fact = IOFactory.getFactory();
+		SerialPort sp = fact.getSerialPort();
+		
+		String hello = "Hello World via Hardware Objects!";
+		
+		// Hello world with low-level device access
+		for (int i=0; i<hello.length(); ++i) {
+			// busy wait on transmit buffer empty
+			while ((sp.status & SerialPort.MASK_TDRE) == 0)
+				;
+			// write a character
+			sp.data = hello.charAt(i);
+		}
+		
+		for (;;) {
+			while (((sp.status) & SerialPort.MASK_RDRF) == 0)
+				;
+			System.out.print((char) sp.data);
+		}
+	}
+}
