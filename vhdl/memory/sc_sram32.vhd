9546252853530bc7f0a7de769e2dc445a35f463d
Martin Schoeberl <martin@jopdesign.com>
2008-07-22 15:12:17 +0000

    correct data out enable

--- vhdl/memory/sc_sram32.vhd
+++ vhdl/memory/sc_sram32.vhd
@@ -228,6 +228,8 @@ begin
 				
 			-- the WS state
 			when wr1 =>
+				ram_nwe <= '0';
+				dout_ena <= '1';
 				ram_ncs <= '0';
 				
 			when wr2 => 
@@ -236,6 +238,7 @@ begin
 				ram_ncs <= '0';
 				
 			when wr3 =>
+				dout_ena <= '1';
 				ram_ncs <= '0';
 
 		end case;
