ff17f9c1526cf59dd21d3ba7215aac945e6d0e70
jwhitham <jwhitham>
2008-08-09 12:29:58 +0000

    vlab directory is now a Python module.

---
+++ xilinx/ml401a/vlab/__init__.py
@@ -0,0 +1,12 @@
+"""
+Virtual Lab Client
+ 
+In order to use this module, you will need to understand 
+Deferreds, a concept which is explained in the Twisted 
+Python documentation. 
+
+Version $Id: __init__.py,v 1.1 2008/08/09 12:29:58 jwhitham Exp $
+"""
+
+from vlab import *
+
