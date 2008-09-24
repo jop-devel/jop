c938b805a2ad96a9dc50151fb5ab16a71f3d9ee1
rasped <rasped>
2008-09-24 13:57:49 +0000

    *** empty log message ***

---
+++ java/target/src/test/wcet/wcadevel/WCADevel.java
@@ -0,0 +1,45 @@
+// Temp test code
+package wcet.wcadevel;
+
+public class WCADevel {
+
+  static public int dowcet() {
+	//A a = new A();
+    A a = new B();
+
+    bar(a);
+
+	return 0;
+  }
+
+   static void bar(A a) {
+       a.foo();
+   }
+
+
+}
+
+//a can now be an A or it can also be a B. So
+//either A.foo() or B.foo() is invoked. We don't know,
+//so we have to consider both possibilities for the WCET
+
+
+// from ms mail
+class A {
+
+   void foo() {
+     int ia = 1;
+   }
+}
+
+class B extends A {
+
+   // overwrites A.foo()
+   void foo() {
+     int ib = 3;
+     ib++;
+     ib--;
+   }
+
+}
+
