7e4664b5ae4f13e9b0cac354aa7933fb7c6d7c58
rasped <rasped>
2008-09-24 13:57:11 +0000

    init

---
+++ java/target/src/test/wcet/StartWCADevel.java
@@ -0,0 +1,21 @@
+// Temp code for testing with MS and RUP and WP
+
+// Put this in the make file
+// P2=wcet
+// P3=StartWCADevel
+// WCET_METHOD=main
+package wcet;
+
+import wcet.wcadevel.WCADevel;
+
+public class StartWCADevel {
+
+
+	public static void main(String[] args) {
+
+		// initialization
+		WCADevel.dowcet();
+
+	}
+
+}
