4e33d62f23327c1f57ac7c88f9c855cc2632e501
9914pich <9914pich>
2009-01-19 09:57:13 +0000

    Verifies static analyzed bytecode WCETs by measurement!

---
+++ java/target/src/test/wcet/BytecodeVerification.java
@@ -0,0 +1,96 @@
+/*
+This file is part of JOP, the Java Optimized Processor
+  see <http://www.jopdesign.com/>
+
+Copyright (C) 2008, Christof Pitter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package wcet;
+
+import java.lang.Math;
+import java.util.Random;
+import com.jopdesign.sys.Const;
+import com.jopdesign.sys.Native;
+
+public class BytecodeVerification {
+
+	final static boolean MEASURE = true;
+	
+	// Shared Variables
+	public static int MAX = 10000; // Number of loops
+	public static int PRIME = 183;
+	public static long SEED = 13;
+	
+	public static void main(String[] args) {		
+		
+		// Initialization for measurement 
+		int cpu_id;
+		
+		int[] array = new int[10];
+		array[0] = 100;
+		
+		System.out.println("Bytecode WCET Verification:");
+
+		cpu_id = Native.rdMem(Const.IO_CPU_ID);
+		if (cpu_id == 0x00000000)
+		{		
+			measure(array);
+		}
+		else
+		{
+			for(;;);
+		}
+	}
+	
+	public static void measure(int [] array) {
+		int test = 0;
+		
+		if (MEASURE){
+			int ts = 0;
+			int te = 0;
+			int to = 0;
+			int random = 0;
+			int error = 0;
+		
+			ts = Native.rdMem(Const.IO_CNT);
+			te = Native.rdMem(Const.IO_CNT);
+			error = te-ts;
+		
+			// Initialization of random generator
+			Random r = new Random(SEED);
+		
+			for(int i=0; i<MAX; i++){
+			
+				// Waiting Time
+				random =  Math.abs(r.nextInt() % PRIME);
+				for(int j=0; j<random; j++);			
+			
+				ts = Native.rdMem(Const.IO_CNT);
+				//test = array[0];
+				array[0] = test;
+				te = Native.rdMem(Const.IO_CNT);
+				to = te-ts-error;
+			
+				if(to>47)
+					System.out.println("Error["+i+"]: " + to + " cycles!");
+			}	
+		}	
+		else{
+			// test = array[0]; // WCET = 28
+			array[0] = test; // WCET = 47
+		}
+	}	
+}
