ba2632f61191077ecd14550604d48323ff182171
Martin Schoeberl <martin@jopdesign.com>
2009-02-28 21:11:12 +0000

    not used

--- bat/cycinit.bat
+++
@@ -1,8 +0,0 @@
-rem not complete just for the start
-rem jbi32 -dDO_PROGRAM=1 -aPROGRAM jbc\je_init.jbc
-rem jbi32 -dDO_PROGRAM=1 -aCONFIGURE jbc\je_init.jbc
-down java\tcpip\Main.jop COM2
-java -cp java/pc Flash java\tcpip\cyc.html
-java -cp java/pc Flash java\tcpip\Main.jop
-java -cp java/pc Flash ttf\jopcyc.ttf
-rem jbi32 -dDO_PROGRAM=1 -aPROGRAM jbc\je_confacx.jbc
--- bat/talinit.bat
+++
@@ -1,8 +0,0 @@
-jbi32 -dDO_PROGRAM=1 -aPROGRAM jbc\acxtal_init.jbc
-jbi32 -dDO_PROGRAM=1 -aCONFIGURE jbc\acxtal_init.jbc
-down java\ejip\Main.jop COM2
-java -cp java/pc Flash java\ejip\test.html 192.168.0.4
-java -cp java/pc Flash java\ejip\Main.jop 192.168.0.4
-java -cp java/pc Flash java\ejip\Tal.class 192.168.0.4
-java -cp java/pc Flash ttf\acxtal_flash.ttf 192.168.0.4
-jbi32 -dDO_PROGRAM=1 -aPROGRAM jbc\acxtal_flash.jbc
