918ba432f47b3de65678f66274c96ad637847bbe
jack <jack@localhost.localdomain>
2009-06-03 12:24:22 +0100

    New version of HWMethTest; measures execution time for various sizes of MAC co-processor.

--- java/target/src/test/test/HWMethTest.java
+++ java/target/src/test/test/HWMethTest.java
@@ -22,47 +22,93 @@ package test;
 
 
 import com.jopdesign.io.JeopardIOFactory;
-import com.jopdesign.sys.Native;
+import com.jopdesign.sys.*;
 
 
 /**
  * @author jack
  *
- * JOP can also say 'Hello World'
  */
 public class HWMethTest {
 
 	public static void main(String[] args) {
-        HWMethTest h = new HWMethTest () ;
-        
-        h.Start () ;
-    }
+    HWMethTest h = new HWMethTest () ;
+    
+    h.Start () ;
+  }
 
 	public void Start() {
-        int mac_size = 10000 ;
-        int [] array1 = new int [ mac_size ] ;
-        int [] array2 = new int [ mac_size ] ;
-        int i, expect, j;
+    final int max_mac_size = 10000 ;
+    final int max_cycles = 100000 ;
+    int [] array1 = new int [ max_mac_size ] ;
+    int [] array2 = new int [ max_mac_size ] ;
 
 
-		System.out.println("Let us begin.");
+    System.out.println("Let us begin.");
 
-        mac_coprocessor m = new mac_coprocessor () ;
+    mac_coprocessor m = new mac_coprocessor () ;
+		int ts = Native.rdMem(Const.IO_CNT);
+		int te = Native.rdMem(Const.IO_CNT);
+		int to = te-ts;
+    int icount = 0 ;
 
-        for ( j = 0 ; j < 10 ; j ++ )
+    for ( int mac_size = 1 ; mac_size < max_mac_size ; icount ++ )
+    {
+      int i, j, time, out, expect = 0;
+      boolean error = false;
+      int max_time = 0;
+      int min_time = 1 << 30;
+      int total_time = 0;
+
+      for ( i = 0 ; i < mac_size ; i ++ )
+      {
+        array1 [ i ] = mac_size + 123 + ( i * 99 ) + ( i * i * 12 ) ;
+        array2 [ i ] = mac_size + 456 + ( i * 78 ) + ( i * i * 9 ) ;
+        expect += array1 [ i ] * array2 [ i ] ;
+      }
+
+      System.out.print(mac_size);
+      System.out.print(" ");
+
+      for ( i = 0 ; i < max_cycles ; i ++ )
+      {
+        ts = Native.rdMem(Const.IO_CNT);
+        out = m.mac1(mac_size, array1, array2);
+        te = Native.rdMem(Const.IO_CNT);
+        time = te - ts - to;
+        if ( time > max_time )
         {
-            // make the data to be mac'ed
-            
-            expect = 0 ;
-            for ( i = 0 ; i < mac_size ; i ++ )
-            {
-                array1 [ i ] = 123 + ( i * ( 99 + j )) + ( i * i * 12 ) ;
-                array2 [ i ] = 456 + ( i * 78 ) + ( i * i * ( 9 + j )) ;
-                expect += array1 [ i ] * array2 [ i ] ;
-            }
-            System.out.print("Test: ");
-            System.out.println(m.mac1(mac_size, array1, array2) - expect);
+          max_time = time;
         }
+        if ( time < min_time )
+        {
+          min_time = time;
+        }
+        total_time += time;
+        if ( out != expect )
+        {
+          error = true;
+          break ;
+        }
+      }
+
+      if ( error )
+      {
+        System.out.println("Unexpected value!");
+        break ;
+      } 
+      System.out.print(min_time);
+      System.out.print(" ");
+      System.out.print(total_time / max_cycles);
+      System.out.print(" ");
+      System.out.println(max_time);
+
+      mac_size ++ ;
+      if ( icount > 100 )
+      {
+        mac_size += mac_size / 4 ;
+      }
+    }
 		System.out.println("That's all.");
 	}
 }
