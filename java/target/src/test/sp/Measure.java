a35173d9ac26b12afca003c9587608e1e68a24e9
Martin Schoeberl <martin@jopdesign.com>
2009-07-11 12:32:52 +0200

    spcmp: TDMA as default, low-level measurements

---
+++ java/target/src/test/sp/Measure.java
@@ -0,0 +1,100 @@
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
+
+  Copyright (C) 2009, Martin Schoeberl (martin@jopdesign.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/**
+ * 
+ */
+package sp;
+
+import com.jopdesign.io.IOFactory;
+import com.jopdesign.io.SysDevice;
+
+/**
+ * Some low-level measurements for single-path CMP programming.
+ * 
+ * @author Martin Schoeberl (martin@jopdesign.com)
+ *
+ */
+public class Measure {
+
+	final static int TDMA_LENGTH = 3*7;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		lowLevel();
+	}
+	
+	public static void lowLevel() {
+
+		SysDevice sys = IOFactory.getFactory().getSysDevice();
+		int frequ = new STScheduler().getMsCycles()*1000;
+		int shift = frequ/10/(TDMA_LENGTH)*TDMA_LENGTH+1;
+		
+		int a[] = new int[5];
+		
+		for (int i=0; i<a.length; ++i) {
+			a[i] = 1;
+		}
+		
+		int time = sys.cntInt;
+		time = sys.cntInt - time;
+		int off = time;
+		
+		int start = sys.cntInt + shift;
+		for (int i=0; i<100; ++i) {
+			sys.deadLine = start;
+			time = sys.cntInt;
+			int sum=0;
+			for (int j=0; j<a.length; ++j) {
+				sum += a[j];
+			}
+			time = sys.cntInt - time;
+			System.out.println(time-off);
+			start += shift;
+		}
+	}
+	
+	public static void sort() {
+
+		SPBubbleSort bs = new SPBubbleSort();
+		SysDevice sys = IOFactory.getFactory().getSysDevice();
+		int frequ = new STScheduler().getMsCycles()*1000;
+		int shift = frequ/10/(TDMA_LENGTH)*TDMA_LENGTH+1;
+		
+		
+		bs.read();
+		
+		int time;
+		int start = sys.cntInt + shift;
+		for (int i=0; i<100; ++i) {
+			sys.deadLine = start;
+			time = sys.cntInt;
+			bs.execute();
+			time = sys.cntInt - time;
+			System.out.println(time);
+			start += shift;
+		}
+
+	}
+
+}
--- java/target/src/test/sp/SPBubbleSort.java
+++ java/target/src/test/sp/SPBubbleSort.java
@@ -1,82 +1,97 @@
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
+
+  Copyright (C) 2009, Raimund Kirner (raimund@vmars.tuwien.ac.at)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // project: spcmp paper
 // example: bubble sort
 // author:  Raimund Kirner, 22.06.2009
-
 
 package sp;
 
 import com.jopdesign.sys.Native;
 
 /**
- * A real-time task that performs sorting via bubble sort.
- * This is a single-path program.
- *
+ * A real-time task that performs sorting via bubble sort. This is a single-path
+ * program.
+ * 
  * @author Raimund Kirner (raimund@vmars.tuwien.ac.at)
  */
-class SPBubbleSort {
-    final int SIZE = 20;
-    int[] aDat; 
-    int i;
-    int j;
-    int temp;
-    boolean cond;
+class SPBubbleSort extends SimpleTask {
+	final int SIZE = 20;
+	int[] aDat;
+	int i;
+	int j;
+	int temp;
+	boolean cond;
 
-    // Standard Constructor 
-    public SPBubbleSort() {
-	aDat = new int[SIZE];
-    }
-
-    // read data from mem
-    public void read() {
-	for (i=0; i < aDat.length; i++) {
-	    aDat[i] = (i + 30) % SIZE;
+	// Standard Constructor
+	public SPBubbleSort() {
+		aDat = new int[SIZE];
 	}
-    } 
 
-    // write data back to mem
-    public void write() {
-	for (i=0; i < aDat.length; i++) {
-            System.out.println("array["+i+"]: "+aDat[i]+";");
+	// read data from mem
+	public void read() {
+		for (i = 0; i < aDat.length; i++) {
+			aDat[i] = (i + 30) % SIZE;
+		}
 	}
-    }
 
-    // sort routine
-    public void sort() {
-	// Sortieren
-	i = 0;
-	while ( i < aDat.length) {  
-	    j = aDat.length - 1;
-	    while ( j > i ) {
-		cond = aDat[j] < aDat[j-1];
-		// conventional swap:
-		//if ( cond==true ) {
-		//    temp  = aDat[j];
-		//    aDat[j]   = aDat[j-1];
-		//    aDat[j-1] = temp;
-		//}
-		//
-		// single path swap:
-		temp = aDat[j];
-		aDat[j]   = Native.condMove(aDat[j-1],aDat[j],cond);
-		aDat[j-1] = Native.condMove(temp,aDat[j-1],cond);
-		j = j-1;
-	    }
-	    i = i+1;
+	// write data back to mem
+	public void write() {
+		for (i = 0; i < aDat.length; i++) {
+			System.out.println("array[" + i + "]: " + aDat[i] + ";");
+		}
 	}
-	
-    }
 
+	// sort routine
+	public void execute() {
+		// Sortieren
+		i = 0;
+		while (i < aDat.length) {
+			j = aDat.length - 1;
+			while (j > i) {
+				cond = aDat[j] < aDat[j - 1];
+				// conventional swap:
+				// if ( cond==true ) {
+				// temp = aDat[j];
+				// aDat[j] = aDat[j-1];
+				// aDat[j-1] = temp;
+				// }
+				//
+				// single path swap:
+				temp = aDat[j];
+				aDat[j] = Native.condMove(aDat[j - 1], aDat[j], cond);
+				aDat[j - 1] = Native.condMove(temp, aDat[j - 1], cond);
+				j = j - 1;
+			}
+			i = i + 1;
+		}
 
-    public static void main(String[] args) {
-	SPBubbleSort spbs = new SPBubbleSort();
+	}
 
-	spbs.read();
+	public static void main(String[] args) {
+		SPBubbleSort spbs = new SPBubbleSort();
 
-	spbs.sort();
+		spbs.read();
 
-	spbs.write();
-    }
+		spbs.execute();
+
+		spbs.write();
+	}
 }
-
-
-
