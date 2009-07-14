56920f0dc3e069a0c5f3b93335fc9e82d87106cf
Raimund Kirner <raimund@vmars.tuwien.ac.at>
2009-07-14 10:05:12 +0200

    update of shared mem

--- java/target/src/test/sp/SharedIMem.java
+++ java/target/src/test/sp/SharedIMem.java
@@ -1,55 +1,111 @@
-// project: spcmp paper
-// example: bubble sort
-// author:  Raimund Kirner, 22.06.2009
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
 
+  Copyright (C) 2009, Martin Schoeberl (martin@jopdesign.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package sp;
 
+import com.jopdesign.sys.Native;
+
 /**
- * A clas to experiment with the Java language.
- * Nothing meaningful in the code.
- *
+ * A class to make wrap around primitive int values to get shared memory elements
+ * (with reference parameter passing). Using a wrapper class like Integer is no
+ * alternative, since the assignment of integer values is only possible via the
+ * constructor, which creates a new reference.
+ * 
  * @author Raimund Kirner (raimund@vmars.tuwien.ac.at)
+ *
  */
-class SharedMem {
-    final int SIZE = 20;
-    Integer IV=5;
-    int i;
+class SharedIMem {
+    int size = 0;
+    int[] dat;
+    int nError = 0;
+    boolean cond;
+    int idx = 0;
+    int tmp1;
+    int tmp;
 
     // Standard Constructor 
-    public SharedMem() {
+    public SharedIMem() {
+	size = 1;
+	dat = new int[size];
     }
 
-    // read data from mem
-    public void read() {
-    } 
-
-    // write data back to mem
-    public void write() {
-	System.out.println(IV);
+    // Constructor with Allocation Size 
+    public SharedIMem(int size) {
+	this.size = size;
+	dat = new int[size];
     }
 
-    // sort routine
-    public void sort() {
+    // return size of shared mem
+    public int size() {
+	return size;
     }
 
-    public void testInteger() {
-	Integer IVar = IV;
-	IVar = 2;
+    // set data index for default access of shared mem
+    public void setIndex(int idx) {
+	cond = (idx >= size);
+	nError   = Native.condMove(1, nError, cond);
+	this.idx = Native.condMove(this.idx, idx, cond);
+    }
+
+    // get data from default index from shared mem
+    public int get() {
+	return dat[idx];
+    }
+
+    // get data from shared mem
+    public int get(int idx) {
+	cond = (idx >= size);
+	nError = Native.condMove(1, nError, cond);
+	tmp1 = Native.condMove(0, idx, cond); // bound index
+	tmp1 = dat[tmp1];
+	tmp = Native.condMove(-1, tmp1, cond);
+	return tmp;
+    }
+
+    // set data at default index in shared mem
+    public void set(int val) {
+	dat[idx] = val;
+    }
+
+    // set data in shared mem
+    public void set(int val, int idx) {
+	cond = (idx >= size);
+	nError = Native.condMove(1, nError, cond);
+	tmp1 = Native.condMove(0, idx, cond); // bound index
+        tmp = dat[tmp1];
+	dat[tmp1] = Native.condMove(tmp, val, cond);
+    }
+
+    // return the error status of the shared mem
+    public boolean error() {
+	return (nError != 0);
+    }
+
+    // resets the error status of the shared mem
+    public void clearError() {
+	nError = 0;
     }
 
 
-    public static void main(String[] args) {
-	RK_Test rk = new RK_Test();
 
-	rk.read();
 
-	rk.testInteger();
-
-	rk.sort();
-
-	rk.write();
-    }
 }
 
 
