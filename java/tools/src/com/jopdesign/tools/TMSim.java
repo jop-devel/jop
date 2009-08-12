19ad3b5760fe9056c03ba2211a57d3059c716121
Peter Hilber <peter@hilber.name>
2009-08-12 12:21:49 +0200

    Revert "rttm: Started changes to TM simulation" on working branch.
    This reverts commit 30dc8fa02e17a39d7c35efb8b96188bf3028d0d1.

--- java/tools/src/com/jopdesign/tools/TMSim.java
+++ java/tools/src/com/jopdesign/tools/TMSim.java
@@ -29,27 +29,17 @@ import java.util.*;
  * Extension of JopSim to simulation real-time transactional memory (RTTM)
  * 
  * @author Martin Schoeberl
- * @author Peter Hilber
  * 
  */
 public class TMSim extends JopSim {
 
-	// write 0 to this address to start and anything else to end transaction
-	// TMTODO use M/LSB only
-	static final int TM_MAGIC = -10000;
-	static final int TM_START_TRANSACTION = 1;
-	static final int TM_END_TRANSACTION = 0;
-	static final int TM_EARLY_COMMIT = 2;	
-	
-	
-	static final boolean LOG = true;
-	static final boolean LOG_ACCESSES = true;
+	final static int MAGIC = -10000;
+	final static boolean LOG = false;
 
 	TMSim(String fn, IOSimMin ioSim, int max) {
 		super(fn, ioSim, max);
 	}
 
-	// Statistics
 	int trCnt;
 	int retryCnt;
 	int maxRead;
@@ -57,48 +47,28 @@ public class TMSim extends JopSim {
 	int maxSum;
 
 	int nestingCnt;
-	boolean inEarlyCommit = false;
 	int savedPc;
 
 	LinkedHashSet<Integer> readSet = new LinkedHashSet<Integer>();
 	LinkedHashMap<Integer, Integer> writeSet = new LinkedHashMap<Integer, Integer>();
-	
-	LinkedHashSet<Integer> readTags = new LinkedHashSet<Integer>();
-	LinkedHashMap<Integer, Integer> writeBuffer = new LinkedHashMap<Integer, Integer>();
-	
-	static final int readTagsCapacity = 128;
-	static final int writeBufferCapacity = 128;
 
-	// TMTODO ? we should earlier abort the transaction as we
-	// can read inconsistent data from another committed transaction
-	// TMTODO ? we're still having constants and method table loads in
+	// TODO: we should earlier abort the transaction as we
+	// can read inconsistent data from another commited transaction
+	// TODO: we're still having constants and method table loads in
 	// the read set, but cache load uses readInstrMem().
 	int readMem(int addr, Access type) {
-		if (LOG_ACCESSES)
-			System.out.println("read: " + addr);
 
-		// TMTODO: use access type
+		// TODO: use access type
 		switch (type) {
 		case MTAB:
 			break;
 		default:
 			// Transaction active and not an I/O address
 			if (nestingCnt > 0 && addr >= 0) {
-				readSet.add(addr);
-				
-				if (!inEarlyCommit) {
-					if (readTags.size() < readTagsCapacity) {
-						readTags.add(addr);
-					} else {
-						earlyCommit();
-					}
-					
-					// TMTODO how is this read after EC?
-					if (writeSet.containsKey(addr)) {
-						return writeSet.get(addr);
-					}
-				} else {
-					return super.readMem(addr, type);
+				Integer addrInt = new Integer(addr);
+				readSet.add(addrInt);
+				if (writeSet.containsKey(addrInt)) {
+					return writeSet.get(addrInt).intValue();
 				}
 			}
 			break;
@@ -108,100 +78,62 @@ public class TMSim extends JopSim {
 	}
 
 	void writeMem(int addr, int data, Access type) {
-		if (LOG_ACCESSES)
-			System.out.println("write: " + addr + " data: " + data);
 
-		if (addr == TM_MAGIC) {
-			switch (data) {
-			case TM_START_TRANSACTION:
+		if (addr == MAGIC) {
+			if (data != 0) {
 				startTransaction();
-				break;
-			case TM_END_TRANSACTION:
+			} else {
 				endTransaction();
-				break;
-			case TM_EARLY_COMMIT:
-				earlyCommit();
-				break;
 			}
 			return;
 		}
 		// Transaction active and not an I/O address
 		if (nestingCnt > 0 && addr >= 0) {
 			writeSet.put(new Integer(addr), new Integer(data));
-			
-			if (inEarlyCommit) {
-				flushWrite(addr, data, type);
-			} else {
-				writeBuffer.put(addr, data);
-			}								
 		} else {
 			super.writeMem(addr, data, type);
 		}
 	}
-	
-	/*
-	 * TMTODO access type may be lost
-	 */
-	void flushWrite(int addr, int data, Access type) {
-		super.writeMem(addr, data, Access.INTERN);
 
-		// test for conflict
-		for (int i = 0; i < nrCpus; ++i) {
-			if (i == io.cpuId)
-				continue;
-			TMSim otherSim = (TMSim) js[i];
-			if (otherSim.abort)
-				continue;
-			if (otherSim.readSet.contains(addr)) {
-				otherSim.abort = true;
-				if (LOG)
-					System.out.println("Transaction on CPU " + i
-							+ " aborted");
-			}
-		}
-	}
-	
-	void internalCommit() {
-		Collection<Integer> keys = writeSet.keySet();
-		
-		for (Iterator<Integer> iterator = keys.iterator(); iterator.hasNext();) {
-			Integer addr = iterator.next();
-			int val = writeSet.get(addr);
-			// TMTODO ? type lost
-			flushWrite(addr, val, Access.INTERN);
-		}
-		
-		// TMTODO
-//		writeBuffer.clear();
-//		readTags.clear();
-	}
-	
-	void earlyCommit() {
-		if (LOG)
-			System.out.println("Early Commit TR " + trCnt + " on CPU " + io.cpuId);
-		
-		// TMTODO early commit is not covered by current simulation
-		
-		inEarlyCommit = true;
-		internalCommit();
-	}
-	
 	void commit() {
 		if (LOG)
-			System.out.print("Committing TR " + trCnt + " on CPU " + io.cpuId);
+			System.out.print("Commiting TR " + trCnt + " on CPU " + io.cpuId);
 		if (LOG)
 			System.out.println(" - write set " + writeSet.size() + " read set "
 					+ readSet.size());
-		
-		inEarlyCommit = false;
-					
 		if (writeSet.size() > maxWrite)
 			maxWrite = writeSet.size();
 		if (readSet.size() > maxRead)
 			maxRead = readSet.size();
 
-		internalCommit();
-		
+		Collection<Integer> keys = writeSet.keySet();
+		for (Iterator<Integer> iterator = keys.iterator(); iterator.hasNext();) {
+			Integer addr = iterator.next();
+			int thisAddr = addr;
+			int val = writeSet.get(addr);
+			// TODO: type lost
+			super.writeMem(thisAddr, val, Access.INTERN);
+
+			// test for conflict
+			for (int i = 0; i < nrCpus; ++i) {
+				if (i == io.cpuId)
+					continue;
+				TMSim otherSim = (TMSim) js[i];
+				if (otherSim.abort)
+					continue;
+				for (Iterator<Integer> other = otherSim.readSet.iterator(); other
+						.hasNext();) {
+					int otherAddr = other.next();
+					if (otherAddr == thisAddr) {
+						otherSim.abort = true;
+						if (LOG)
+							System.out.println("Transaction on CPU " + i
+									+ " aborted");
+						break;
+					}
+				}
+			}
+		}
 		// find the sum of different addresses
 		for (Iterator<Integer> it = readSet.iterator(); it.hasNext();) {
 			int addr = it.next();
@@ -209,25 +141,18 @@ public class TMSim extends JopSim {
 		}
 		if (writeSet.size() > maxSum)
 			maxSum = writeSet.size();
-
 		writeSet.clear();
 		readSet.clear();
-		writeBuffer.clear();
-		readTags.clear();
 	}
 
 	void retry() {
 		abort = false;
 		pc = savedPc;
 		// also restore the stack for the write
-		stack[++sp] = 1; // TMTODO
-		stack[++sp] = TM_MAGIC;
-
+		stack[++sp] = 1;
+		stack[++sp] = MAGIC;
 		writeSet.clear();
 		readSet.clear();
-		writeBuffer.clear();
-		readTags.clear();
-		
 		if (LOG)
 			System.out.println("Retry TR " + trCnt + " on CPU " + io.cpuId);
 		--trCnt;
@@ -238,7 +163,7 @@ public class TMSim extends JopSim {
 	 * The single commit token. 0 means free otherwise it contains the CPU ID +
 	 * 1.
 	 */
-	static int committingCpu;
+	static int commitingCpu;
 	boolean abort;
 
 	void startTransaction() {
@@ -260,17 +185,16 @@ public class TMSim extends JopSim {
 			if (abort) {
 				retry();
 			} else {
-				if (committingCpu != 0) {
-					// wait for commit token ~by repeating write~
-					// TMTODO how would this work in HW?
+				if (commitingCpu != 0) {
+					// wait for commit token
 					--pc;
 					stack[++sp] = 0;
-					stack[++sp] = TM_MAGIC;
+					stack[++sp] = MAGIC;
 					System.out.println("wait for token");
 				} else {
-					committingCpu = io.cpuId + 1;
+					commitingCpu = io.cpuId + 1;
 					commit();
-					committingCpu = 0;
+					commitingCpu = 0;
 				}
 			}
 		}
