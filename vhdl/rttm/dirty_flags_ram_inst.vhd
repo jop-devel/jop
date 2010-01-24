cfdc384784c6b27a3c4d8d82fea38b5071977a2b
Peter Hilber <peter@hilber.name>
2010-01-24 17:38:07 +0100

    rttm: Example megafunction instantiations.

---
+++ vhdl/rttm/dirty_flags_ram_inst.vhd
@@ -0,0 +1,8 @@
+dirty_flags_ram_inst : dirty_flags_ram PORT MAP (
+		clock	 => clock_sig,
+		data	 => data_sig,
+		rdaddress	 => rdaddress_sig,
+		wraddress	 => wraddress_sig,
+		wren	 => wren_sig,
+		q	 => q_sig
+	);
