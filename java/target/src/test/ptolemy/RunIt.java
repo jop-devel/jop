efdd3fa63873989e5525cabf2c83d9eab5db3db6
Martin Schoeberl <martin@jopdesign.com>
2010-02-05 23:05:19 +0100

    more for the ptolemy evaluation

--- java/target/src/test/ptolemy/RunIt.java
+++ java/target/src/test/ptolemy/RunIt.java
@@ -33,7 +33,7 @@ import joprt.RtThread;
  */
 public class RunIt {
 
-	static Model model = new Model();
+	// static Model model = new Model();
 	// static WatchDog model = new WatchDog();
 	// static WriteOutput model = new WriteOutput();
 	// static ReadInput model = new ReadInput();
@@ -43,6 +43,7 @@ public class RunIt {
 	// static FsmDemo model = new FsmDemo();
 	// static Simple model = new Simple();
 	// static LightFollower model = new LightFollower();
+	static LineFollower model = new LineFollower();
 
 	/**
 	 * Create and run a Ptolemy model.
