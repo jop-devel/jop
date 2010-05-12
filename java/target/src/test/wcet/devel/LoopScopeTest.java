b3c5aba15e9e61367e8446b66a15bc318e04a323
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2010-05-12 19:05:04 +0200

    wcet/ocache: A test case for the handling of scopes in the analysis.

---
+++ java/target/src/test/wcet/devel/LoopScopeTest.java
@@ -0,0 +1,56 @@
+package wcet.devel;
+
+public class LoopScopeTest {
+
+	public static void main(String [] args) {
+		Big big = new Big();		
+		measure(big);
+	}
+
+	public static void measure(Big big) {
+		loop(big);
+		loopedaccess(big);
+	}
+
+	public static void loop(Big big) {
+		for (int k = 0; k < 8; k++) {
+			if (big != null) {
+				access(big);
+			}
+			big = big.next;
+		}
+	}
+
+	public static void access(Big big) {
+		int x = big.a + big.b + big.c + big.d;
+		int y = big.e + big.f + big.g + big.h;
+		int z = big.h + big.d + big.g + big.c;
+		int w = big.f + big.b + big.e + big.a;	
+	}
+
+	public static void loopedaccess(Big big) {
+		for (int k = 0; k < 8; k++) {
+			if (big != null) {
+				int x = big.a + big.b + big.c + big.d;
+				int y = big.e + big.f + big.g + big.h;
+				int z = big.h + big.d + big.g + big.c;
+				int w = big.f + big.b + big.e + big.a;
+			}
+			big = big.next;
+		}
+		
+	}
+
+}
+
+class Big {
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	Big next;
+}
