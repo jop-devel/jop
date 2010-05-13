4c44cac45be7fbf3bfe88a1ab2434c9acdb64f6c
Martin Schoeberl <martin@jopdesign.com>
2010-05-13 13:13:07 +0200

    wcet/ocache: work on examples

--- ocwcet.sh
+++ ocwcet.sh
@@ -1,5 +1,36 @@
 #! /bin/sh
 
+#make java_app wcet P1=bench P2=scd_micro P3=Main WCET_METHOD="scd_micro.Motion.findIntersection(Lscd_micro/Motion)" \
+make java_app wcet P1=bench P2=scd_micro P3=Main WCET_METHOD="measure" \
+  TARGET_JDK=jdk16mod \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+exit;
+
+make java_app wcet P1=test P2=wcet/devel P3=Simple WCET_METHOD=measure3 \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis -jop-ocache-line-size 1"
+
+exit;
+
+make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis -jop-ocache-line-size 1"
+
+exit;
+
+make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+
+make java_app wcet P1=common P2=ttpa/demo P3=Main WCET_METHOD=ttpa.protocol.Node.doMpSlotAction \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+
+make java_app wcet P1=paper/trading/plain P2=com/sun/oss/trader P3=Main WCET_METHOD="com.sun.oss.trader.tradingengine.OrderManager.checkForTrade(Lcom/sun/oss/trader/data/OrderEntry;D)Z" \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+
+
+# jolden is not in the source
 make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
   WCET_OPTIONS="-dataflow-analysis -object-cache-analysis -jop-ocache-line-size 1"
 
