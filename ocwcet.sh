91347f0bf1d7ce186985d093fb9fb0f35e532bae
Martin Schoeberl <martin@jopdesign.com>
2010-05-25 10:38:04 +0200

    oc: run script changes

--- ocwcet.sh
+++ ocwcet.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
+
+make java_app wcet P1=paper/trading/plain P2=com/sun/oss/trader P3=Main WCET_METHOD="com.sun.oss.trader.tradingengine.OrderManager.checkForTrade(Lcom/sun/oss/trader/data/OrderEntry;D)Z" \
+  TARGET_JDK=jdk16mod \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+
+exit;
+
+
+make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
+  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
+
+exit;
+
 #make java_app wcet P1=bench P2=scd_micro P3=Main WCET_METHOD="scd_micro.Motion.findIntersection(Lscd_micro/Motion)" \
 make java_app wcet P1=bench P2=scd_micro P3=Main WCET_METHOD="measure" \
   TARGET_JDK=jdk16mod \
@@ -17,8 +31,6 @@ make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
 
 exit;
 
-make java_app wcet P1=test P2=wcet P3=StartLift WCET_METHOD=measure \
-  WCET_OPTIONS="-dataflow-analysis -object-cache-analysis"
 
 
 make java_app wcet P1=common P2=ttpa/demo P3=Main WCET_METHOD=ttpa.protocol.Node.doMpSlotAction \
