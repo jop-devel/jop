ae76d89b9fae621ae5a650daa0f120fe420c4e07
Benedikt Huber <benedikt.huber@gmail.com>
2010-08-10 16:23:27 +0200

    wcet/test: Add a LineFollower benchmark, where the loop is executed 100 times (instructions stay in the cache)

---
+++ java/target/src/test/wcet/StartLineFollower100.java
@@ -0,0 +1,67 @@
+/*
+  This file is part of JOP, the Java Optimized Processor
+    see <http://www.jopdesign.com/>
+
+  Copyright (C) 2001-2008, Martin Schoeberl (martin@jopdesign.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package wcet;
+
+import lego.LineFollower;
+
+import com.jopdesign.sys.*;
+
+public class StartLineFollower100 {
+
+	/**
+	 * Set to false for the WCET analysis, true for measurement
+	 */
+	final static boolean MEASURE = false;
+        static int ITERATIONS = 100;
+	static int ts, te, to;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		ts = Native.rdMem(Const.IO_CNT);
+		te = Native.rdMem(Const.IO_CNT);
+		to = te-ts;
+		LineFollower.init();
+		invoke();
+		if (MEASURE) {
+                    System.out.print("wcet:");
+                    System.out.println(te-ts-to);
+                }
+	}
+	
+	static void invoke() {
+		measure();
+		if (MEASURE) te = Native.rdMem(Const.IO_CNT);
+	}
+
+	static void measure() {
+		if (MEASURE) ts = Native.rdMem(Const.IO_CNT);
+                loop();
+	}
+        static void loop() {
+            for(int i = 0; i < ITERATIONS; i++) {
+                LineFollower.loop();
+            }
+        }
+	
+}
