14418d70d166e25a0f0e8548b5ece0e6a47bee4e
Stefan Hepp <stefan@stefant.org>
2010-08-11 21:44:53 +0200

    test commit

--- java/newtools/src/com/jopdesign/common/FieldInfo.java
+++ java/newtools/src/com/jopdesign/common/FieldInfo.java
@@ -27,6 +27,8 @@ import org.apache.bcel.classfile.Field;
 import org.apache.bcel.generic.FieldGen;
 
 /**
+ * Class for a field member of a ClassInfo.
+ *  
  * @author Stefan Hepp (stefan@stefant.org)
  */
 public final class FieldInfo extends ClassMemberInfo {
