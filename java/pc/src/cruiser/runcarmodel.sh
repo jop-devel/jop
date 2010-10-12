699b5ae028cc8c55b64cdf480f70b21ba360d24e
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2010-10-12 11:15:29 +0200

    Moving cruiser car model to more suitable directory.

---
+++ java/pc/src/cruiser/runcarmodel.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+rm -rf dist/
+mkdir dist
+javac -Xlint:all -sourcepath ..:../../../target/src/bench \
+	model/Main.java -d dist
+stty -F /dev/ttyS0 115200 raw -echo
+tee input.log < /dev/ttyS0 | java -cp dist cruiser/model/Main 2> test.log | tee output.log > /dev/ttyS0
