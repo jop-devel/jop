5442188f8fdfe3c9139285cdac8e3819a7710d74
Martin Schoeberl <martin@jopdesign.com>
2011-01-07 01:28:05 +0000

    ocache: multiple objects and fields work

--- vhdl/cache/ocache.vhd
+++ vhdl/cache/ocache.vhd
@@ -29,6 +29,8 @@
 --	2011-01-06	multiple objects, single field (index 0) per object works
 --		Simulation and usbmin is ok, usb100 crashes -- a fmax problem?
 --		without GC redirection in mem_sc, usb100 is ok (GC crashes)
+--	2011-01-07	multiple objects and fields works
+--		TODO: check with ModelSim if all is ok
 --
 
 --
@@ -56,7 +58,7 @@ package oc_types is
 
 	type oc_tag_out_type is record
 		hit         : std_logic; -- field hit
---		hit_tag		: out std_logic; -- just tag hit
+		hit_tag		: std_logic; -- tag hit
 		hit_line    : unsigned(OCACHE_WAY_BITS-1 downto 0); -- cache line on a hit
 		nxt_line    : unsigned(OCACHE_WAY_BITS-1 downto 0); -- next cache line to use on a miss
 		
@@ -104,8 +106,10 @@ architecture rtl of oc_tag is
 
 	constant line_cnt: integer := 2**OCACHE_WAY_BITS;
 	constant FIELD_CNT: integer := 2**OCACHE_INDEX_BITS;
+	constant VALID_ZERO: std_logic_vector(FIELD_CNT-1 downto 0) := (others => '0');
+	
 
-	signal hit: std_logic;
+--	signal hit: std_logic;
 	signal line: unsigned(OCACHE_WAY_BITS-1 downto 0);
 	signal idx, wridx: unsigned(OCACHE_INDEX_BITS-1 downto 0);
 
@@ -122,7 +126,6 @@ architecture rtl of oc_tag is
 
 begin
 
-	tag_out.hit <= hit;
 	tag_out.hit_line <= line;
 	tag_out.nxt_line <= nxt;
 	
@@ -132,6 +135,8 @@ begin
 	process(tag, tag_in.addr, valid, idx)
 		variable h: std_logic_vector(line_cnt-1 downto 0);
 		variable h_or: std_logic;
+		variable th: std_logic_vector(line_cnt-1 downto 0);
+		variable th_or: std_logic;
 		variable n: unsigned(OCACHE_WAY_BITS-1 downto 0);
 		variable v: std_logic_vector(FIELD_CNT-1 downto 0);
 	begin
@@ -139,22 +144,32 @@ begin
 		-- hit detection
 		-- TODO: tag and valid detection
 		h := (others => '0');
+		th := (others => '0');
 		for i in 0 to line_cnt-1 loop
 			-- TODO: we can remove v again and
 			-- use valid(i)(to_integer(idx))
 			v := valid(i);
-			if tag(i)= tag_in.addr 
-				and v(to_integer(idx))='1' then
-				h(i) := '1';
+			if tag(i)= tag_in.addr then
+				-- hit on valid field
+				if v(to_integer(idx))='1' then
+					h(i) := '1';
+				end if;
+				-- tag is valid if any field is valid
+				if v /= VALID_ZERO then
+					th(i) := '1';
+				end if;
 			end if;
 		end loop;
 
 		h_or := '0';
+		th_or := '0';
 		for i in 0 to line_cnt-1 loop
 			h_or := h_or or h(i);
+			th_or := th_or or th(i);
 		end loop;
 		-- ignore uncacheable index values, it is checked in ocache
-		hit <= h_or;
+		tag_out.hit <= h_or;
+		tag_out.hit_tag <= th_or;
 		
 		-- check if index is in the cachable area
 --		if tag_in.index(OCACHE_MAX_INDEX_BITS-1 downto OCACHE_INDEX_BITS) = (others => '0') then
@@ -168,7 +183,7 @@ begin
 		for i in 0 to OCACHE_WAY_BITS-1 loop
 			for j in 0 to line_cnt-1 loop
 				n := to_unsigned(j, OCACHE_WAY_BITS);
-				if n(i)='1' and h(j)='1' then
+				if n(i)='1' and th(j)='1' then
 					line(i) <= '1';
 				end if;
 			end loop;
@@ -194,8 +209,11 @@ begin
 			-- update tag memory when data is available
 			if tag_in.wr='1' then
 				tag(to_integer(tag_in.wrline)) <= tag_in.wraddr;
-				-- TODO: only reset other valid signals on a new tag
-				v := (others => '0');
+				v := valid(to_integer(tag_in.wrline));
+				-- only reset other valid signals on a new tag
+				if tag_in.inc_nxt='1' then
+					v := (others => '0');
+				end if;
 				v(to_integer(wridx)) := '1';
 				valid(to_integer(tag_in.wrline)) <= v; 
 			end if;
@@ -244,7 +262,7 @@ architecture rtl of ocache is
 		:= (others => '0');
 	signal ocin_reg: ocache_in_type;
 	signal line_reg: unsigned(OCACHE_WAY_BITS-1 downto 0);
-	signal hit_reg: std_logic;
+	signal hit_reg, hit_tag_reg: std_logic;
 	signal inc_nxt_reg: std_logic;
 
 	signal cacheable, cacheable_reg: std_logic;
@@ -286,7 +304,7 @@ begin
 	oc_tag_in.wr <= update_cache;
 	
 -- check for cachable fields 
-process(ocin, hit_reg, inc_nxt_reg, cacheable_reg)
+process(ocin, hit_reg, hit_tag_reg, inc_nxt_reg, cacheable_reg)
 begin
 
 	cacheable <= '0';
@@ -299,7 +317,7 @@ begin
 	-- update_cache is used by the data memory too
 	-- we could split into: update_data, update_tag, update_valid
 	update_cache <= '0';
-	if ocin.wr_gf='1' or (ocin.wr_pf='1' and hit_reg='1') then
+	if ocin.wr_gf='1' or (ocin.wr_pf='1' and hit_tag_reg='1') then
 		-- update only on valid index
 		if cacheable_reg='1' then
 			update_cache <= '1';
@@ -307,6 +325,8 @@ begin
 	end if;
 	-- increment nxt pointer on a miss
 	-- TODO: change when caching several fields
+	--		should be ok now as it is in inc_nxt_reg considered
+	-- TODO: adaption needed for write allocate
 	oc_tag_in.inc_nxt <= '0';
 	if ocin.wr_gf='1' and cacheable_reg='1' then -- that's a getfield miss (now)
 		oc_tag_in.inc_nxt <= inc_nxt_reg;
@@ -347,6 +367,7 @@ begin
 		oc_tag_in.invalidate <= '1';
 		chk_gf_dly <= '0';
 		hit_reg <= '0';
+		hit_tag_reg <= '0';
 		cacheable_reg <= '0';
 		line_reg <= (others => '0');
 		ram_dout_store <= (others => '0');
@@ -361,6 +382,7 @@ begin
 		-- remember handle, index, and if it was a hit
 		if ocin.chk_gf='1' or ocin.chk_pf='1' then
 			hit_reg <= oc_tag_out.hit and cacheable;
+			hit_tag_reg <= oc_tag_out.hit_tag and cacheable;
 			ocin_reg <= ocin;
 			cacheable_reg <= cacheable;
 		end if;
@@ -368,11 +390,17 @@ begin
 		-- chk_gf will result (at the moment) in a wr_gf on
 		-- a miss
 		if ocin.chk_gf='1' then
-			line_reg <= oc_tag_out.nxt_line;
-			-- nxt increment only on a getfield miss
-			inc_nxt_reg <= '1';
+			if oc_tag_out.hit_tag='1' then
+				line_reg <= oc_tag_out.hit_line;
+				inc_nxt_reg <= '0';
+			else
+				line_reg <= oc_tag_out.nxt_line;
+				-- nxt increment only on a tag miss
+				inc_nxt_reg <= '1';
+			end if;
 		end if;
 		-- putfield update only on a hit
+		-- TODO: needs adaption for write allocate
 		if ocin.chk_pf='1' then
 			line_reg <= oc_tag_out.hit_line;
 			inc_nxt_reg <= '0';
