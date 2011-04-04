09ddc652b67604f0d69b6646526d197648a06e26
Benedikt Huber <benedikt.huber@gmail.com>
2011-04-05 00:35:58 +0200

    dfa/test: two simple tests for the loop bound analysis, the second one works now thanks to > commit 6cf54acb9de76a9c1f7f8bda7f6d81d0302c66c2 > Author: Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at> >    dfa: Tweak ordering of worklist to improve scoping in loop bound analysis.

--- java/target/src/test/wcet/devel/DfaLoopBound1.java
+++ java/target/src/test/wcet/devel/DfaLoopBound1.java
@@ -7,18 +7,32 @@ import com.jopdesign.sys.Native;
 
 public class DfaLoopBound1 {
 	static volatile int x;
+	/* most simple DFA test (7 * 10K) */
+	private static void test0() {
+		int u = 9;
+		for(int i = 0; i+1 < u-1; ++i) {
+			compute1();
+		}
+	}
+	/* basic DFA test (5 * 10K) */
 	private static void test1() {
 		int u,l;
-		/* basic DFA test */
 		if((x&1) == 0) l=2;
 		else         l=3;
 		if(((x>>1) & 1) == 0) u=5;
 		else                  u=7;
 		for(int i = l; i < u; ++i) {
-			x = x * 31;
+			compute1();
 		}
 	}
 
+	/* should have roughly 10K cycles to simplify the evaluation */
+	static void compute1()  { 
+		for(int j=0;j<1;++j)    // @WCA loop=1
+		for(int i= 7;i<200;++i) // @WCA loop=193
+			x = (x+1) * i;
+	}
+	
 	/* testing wcet */
 	/** Set to false for the WCET analysis, true for measurement */
 	final static boolean MEASURE = true;
@@ -44,6 +58,7 @@ public class DfaLoopBound1 {
 
 	static void measure() {
 		if (MEASURE) ts = Native.rdMem(Const.IO_CNT);
+		test0();
 		test1();
 	}
 
