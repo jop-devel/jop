76c6cd1bfe92ed23a29328a71d89aa3104cefe81
Wolfgang Puffitsch <wpuffits@mail.tuwien.ac.at>
2011-04-20 14:57:23 +0200

    newtools: Add test case for correct resolution of interface methods.

---
+++ java/target/src/test/wcet/devel/InterfaceInherit.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of JOP, the Java Optimized Processor
+ *   see <http://www.jopdesign.com/>
+ *
+ * Copyright (C) 2011, Wolfgang Puffitsch
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package wcet.devel;
+
+public class InterfaceInherit {
+
+	public static void main(String [] args) {
+		I b = new B();
+		b.foo();
+	}
+}
+
+class A {
+	public void foo() {
+	}
+}
+
+interface I {
+	public void foo();
+}
+
+class B extends A implements I {
+}
