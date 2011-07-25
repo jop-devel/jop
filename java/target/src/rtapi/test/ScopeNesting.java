985aefbece7f7c9eeeb2b243d5c035f12f3a3cc0
Martin Schoeberl <martin@jopdesign.com>
2011-07-25 11:47:52 +0200

    scj: fix nested scope example

--- java/target/src/rtapi/test/ScopeNesting.java
+++ java/target/src/rtapi/test/ScopeNesting.java
@@ -16,58 +16,63 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package test;
 
 import com.jopdesign.sys.Memory;
 
 public class ScopeNesting {
 
-	   Memory scMemory = new Memory(4096);
+	Memory scMemory = new Memory(4096);
 
-	   public void run(){
+	public void run() {
 
-	       MyLogic logic = new MyLogic();
+		MyLogic logic = new MyLogic();
 
-	       // When the enter() method returns, all the objects created within
-	       // the run() method of the MyLogic instance should be removed, including the
-	       // scoped memory areas created in logic.run().
+		// When the enter() method returns, all the objects created within
+		// the run() method of the MyLogic instance should be removed, including
+		// the
+		// scoped memory areas created in logic.run().
 
-	       for(int i=0; i<10000; i++){
+		for (int i = 0; i < 10000; i++) {
 
-	    	   // println(i) generates garbage
-	           System.out.print(i);
-	           System.out.println();
-	           scMemory.enter(logic);
-	       }
-	   }
+			// println(i) generates garbage
+			System.out.print(i);
+			System.out.println();
+			scMemory.enter(logic);
+		}
+	}
 
-	   public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	       ScopeNesting myApp = new ScopeNesting();
-	       myApp.run();
-	   }
+		ScopeNesting myApp = new ScopeNesting();
+		myApp.run();
+	}
 
-	   class MyLogic implements Runnable{
+	class MyLogic implements Runnable {
 
-	       @Override
-	       public void run() {
-	           // TODO Auto-generated method stub
-	           System.out.println("A");
-	           Memory nestScMem = new Memory(512,512);
-	           MyLogic_2 logic_2 = new MyLogic_2();
-	           nestScMem.enter(logic_2);
-	       }
-	   }
+		@Override
+		public void run() {
+			System.out.println("A");
+			MyLogic_2 logic_2 = new MyLogic_2();
+			// Ms: nested scopes need to entered with enterPrivateMemory
+			// otherwise we are not reusing the backing store and run
+			// out of backing store
+			// Memory nestScMem = new Memory(512,512);
+			// nestScMem.enter(logic_2);
+			Memory m = Memory.getCurrentMemory();
+			m.enterPrivateMemory(512, logic_2);
 
-	   class MyLogic_2 implements Runnable{
+		}
+	}
 
-	       @Override
-	       public void run() {
-	           // TODO Auto-generated method stub
-	           System.out.println("B");
-	       }
-	   }
+	class MyLogic_2 implements Runnable {
 
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			System.out.println("B");
+		}
+	}
 
 }
