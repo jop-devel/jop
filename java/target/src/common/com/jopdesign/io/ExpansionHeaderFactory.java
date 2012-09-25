b895bd613827bdd271dd7c9d40a24c45b219d736
Tórur Biskopstø Strøm <torur.strom@gmail.com>
2012-09-25 14:32:11 +0200

    reprap:Fixed encoding

--- java/target/src/common/com/jopdesign/io/ExpansionHeaderFactory.java
+++ java/target/src/common/com/jopdesign/io/ExpansionHeaderFactory.java
@@ -1,8 +1,5 @@
 /*
-  This file is part of JOP, the Java Optimized Processor
-    see <http://www.jopdesign.com/>
-
-  Copyright (C) 2001-2008, Martin Schoeberl (martin@jopdesign.com)
+  Copyright (C) 2012, Tórur Biskopstø Strøm (torur.strom@gmail.com)
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -16,10 +13,6 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-	Author: T�rur Biskopst� Str�m (torur.strom@gmail.com)
 */
 
 package com.jopdesign.io;
