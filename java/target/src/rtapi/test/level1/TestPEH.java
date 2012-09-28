357500fe33d6abe5e120c4a190526848f7b0bf78
jrri <jrri@imm.dtu.dk>
2012-09-28 14:58:11 +0200

    scj: Updated simple test for AEH

--- java/target/src/rtapi/test/level1/TestPEH.java
+++ java/target/src/rtapi/test/level1/TestPEH.java
@@ -1,5 +1,7 @@
 package test.level1;
 
+import java.util.Random;
+
 import javax.realtime.PeriodicParameters;
 import javax.realtime.PriorityParameters;
 import javax.safetycritical.AperiodicEventHandler;
@@ -10,8 +12,10 @@ import javax.safetycritical.Terminal;
 
 public class TestPEH extends PeriodicEventHandler {
 	
-	int count = 0;
+	int fireCount = 0;
 	AperiodicEventHandler aeh;
+	
+	Random rnd = new Random();
 
 	public TestPEH(PriorityParameters priority,
 			PeriodicParameters parameters, StorageParameters scp, long scopeSize, AperiodicEventHandler aeh) {
@@ -22,16 +26,16 @@ public class TestPEH extends PeriodicEventHandler {
 	@Override
 	public void handleAsyncEvent() {
 		
-		if (count < 5){
-			Terminal.getTerminal().writeln("PEH");
-			count++;
+		Terminal.getTerminal().writeln("PEH");
+
+		if (rnd.nextInt(3) == 1){
 			aeh.release();
-			
-		}else{
-			Mission.getCurrentMission().requestTermination();
+			fireCount++;
 		}
 		
-		
+		if (fireCount > 2){
+			Mission.getCurrentMission().requestTermination();
+		}
 		
 	}
 
