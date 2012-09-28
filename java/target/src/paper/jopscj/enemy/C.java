e704608cc143cd753ad25b1c80a984c4aa0c4cc5
jrri <jrri@imm.dtu.dk>
2012-09-28 14:52:55 +0200

    scj: Added simple example of the "friend" concept in java

---
+++ java/target/src/paper/jopscj/enemy/C.java
@@ -0,0 +1,15 @@
+package jopscj.enemy;
+
+import jopscj.friend1.A;
+
+public class C {
+	
+	
+	public static void oneMethod(){
+		
+		// Can not access other than public methods
+		A.baz();
+		
+	}
+
+}
